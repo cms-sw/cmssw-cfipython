0e617a491903549ce14a59f1ad36b4b2fa23dab6
Cms Build <cmsbuild@cern.ch>
2018-06-08 04:55:58 +0200

    Updated for CMSSW_10_1_X_2018-06-07-2300

--- HLTrigger/special/hltPixelActivityFilter_cfi.py
+++ HLTrigger/special/hltPixelActivityFilter_cfi.py
@@ -5,6 +5,8 @@ hltPixelActivityFilter = cms.EDFilter('HLTPixelActivityFilter',
   inputTag = cms.InputTag('hltSiPixelClusters'),
   minClusters = cms.uint32(3),
   maxClusters = cms.uint32(0),
-  minLayers = cms.uint32(0),
-  maxLayers = cms.uint32(0)
+  minLayersBPix = cms.uint32(0),
+  maxLayersBPix = cms.uint32(0),
+  minLayersFPix = cms.uint32(0),
+  maxLayersFPix = cms.uint32(0)
 )
