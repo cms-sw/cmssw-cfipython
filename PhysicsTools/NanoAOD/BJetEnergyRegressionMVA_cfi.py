86932cebc6966ea272ee1a672939e54166fffd57
Cms Build <cmsbuild@cern.ch>
2018-07-09 12:53:05 +0200

    Updated for CMSSW_10_2_X_2018-07-09-1100

--- PhysicsTools/NanoAOD/BJetEnergyRegressionMVA_cfi.py
+++ PhysicsTools/NanoAOD/BJetEnergyRegressionMVA_cfi.py
@@ -1,5 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 BJetEnergyRegressionMVA = cms.EDProducer('BJetEnergyRegressionMVA',
-  variables = cms.PSet()
+  variables = cms.PSet(),
+  backend = cms.string('TMVA'),
+  inputTensorName = cms.string(''),
+  outputTensorName = cms.string(''),
+  outputNames = cms.vstring(),
+  outputFormulas = cms.vstring(),
+  nThreads = cms.uint32(1),
+  singleThreadPool = cms.string('no_threads')
 )
