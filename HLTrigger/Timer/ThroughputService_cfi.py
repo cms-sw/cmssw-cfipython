6f77ca720cd7416727304136c685bc29a5468e0a
Cms Build <cmsbuild@cern.ch>
2018-07-11 05:28:19 +0200

    Updated for CMSSW_10_2_X_2018-07-10-2300

--- HLTrigger/Timer/ThroughputService_cfi.py
+++ HLTrigger/Timer/ThroughputService_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 ThroughputService = cms.Service('ThroughputService',
   timeRange = cms.untracked.double(60000),
   timeResolution = cms.untracked.double(10),
-  dqmPath = cms.untracked.string('HLT/Throughput')
+  dqmPath = cms.untracked.string('HLT/Throughput'),
+  dqmPathByProcesses = cms.untracked.bool(False)
 )
