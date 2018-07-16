d9286432f908a9be6b2fbab0a7215b83216c1d31
Cms Build <cmsbuild@cern.ch>
2018-07-16 12:35:22 +0200

    Updated for CMSSW_10_1_X_2018-07-16-1100

--- EventFilter/SiStripRawToDigi/SiStripDigiToRawModule_cfi.py
+++ EventFilter/SiStripRawToDigi/SiStripDigiToRawModule_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 SiStripDigiToRawModule = cms.EDProducer('SiStripDigiToRawModule',
-  InputModuleLabel = cms.string('simSiStripDigis'),
-  InputDigiLabel = cms.string('ZeroSuppressed'),
   FedReadoutMode = cms.string('ZERO_SUPPRESSED'),
+  PacketCode = cms.string('ZERO_SUPPRESSED'),
   UseFedKey = cms.bool(False),
   UseWrongDigiType = cms.bool(False),
   CopyBufferHeader = cms.bool(False),
+  InputDigis = cms.InputTag('simSiStripDigis', 'ZeroSuppressed'),
   RawDataTag = cms.InputTag('rawDataCollector')
 )
