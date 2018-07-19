40957f081b7fac8ee55139de47544034e50ba9fe
Cms Build <cmsbuild@cern.ch>
2018-07-19 04:49:47 +0200

    Updated for CMSSW_10_3_X_2018-07-18-2300

--- Validation/HGCalValidation/hgcalGeometryTest_cfi.py
+++ Validation/HGCalValidation/hgcalGeometryTest_cfi.py
@@ -11,5 +11,12 @@ hgcalGeometryTest = cms.EDAnalyzer('HGCGeometryTest',
     'HGCHitsHEfront',
     'HGCHitsHEback'
   ),
-  Verbosity = cms.untracked.int32(0)
+  RMin = cms.untracked.double(0),
+  RMax = cms.untracked.double(3000),
+  ZMin = cms.untracked.double(3000),
+  ZMax = cms.untracked.double(6000),
+  NBinR = cms.untracked.int32(300),
+  NBinZ = cms.untracked.int32(300),
+  Verbosity = cms.untracked.int32(0),
+  IfNose = cms.untracked.bool(False)
 )
