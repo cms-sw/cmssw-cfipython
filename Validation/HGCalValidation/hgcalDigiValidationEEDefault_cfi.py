f2a14fa294c87027d12d58f1c6f125a6fdf0edfa
Cms Build <cmsbuild@cern.ch>
2018-09-09 14:19:10 +0200

    Updated for CMSSW_10_3_X_2018-09-09-0000

--- Validation/HGCalValidation/hgcalDigiValidationEEDefault_cfi.py
+++ Validation/HGCalValidation/hgcalDigiValidationEEDefault_cfi.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hgcalDigiValidationEEDefault = cms.EDProducer('HGCalDigiValidation',
   DetectorName = cms.string('HGCalEESensitive'),
   DigiSource = cms.InputTag('hgcalDigis', 'EE'),
+  ifNose = cms.bool(False),
   ifHCAL = cms.bool(False),
   Verbosity = cms.untracked.int32(0),
   SampleIndx = cms.untracked.int32(0)
