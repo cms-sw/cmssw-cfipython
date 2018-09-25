e7f12dcf79eb9cfb36c1065777e02d8e0994cbb6
Cms Build <cmsbuild@cern.ch>
2018-09-25 07:22:16 +0200

    Updated for CMSSW_10_3_X_2018-09-24-2300

--- FWCore/Integration/tabletestOutput_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-tabletestOutput = cms.OutputModule('edmtest::TableTestOutputModule',
-  outputCommands = cms.untracked.vstring('keep *'),
-  SelectEvents = cms.untracked.PSet()
-)
--- FWCore/Integration/thingProd_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-thingProd = cms.EDProducer('ThingProducer',
-  offsetDelta = cms.int32(0),
-  nThings = cms.int32(20),
-  noPut = cms.untracked.bool(False)
-)
