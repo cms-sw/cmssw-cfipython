d0cfc601e9dbd79a88a9a640e868a0fe373ac862
Cms Build <cmsbuild@cern.ch>
2018-11-29 00:42:36 +0100

    Updated for CMSSW_10_4_X_2018-11-28-2300

---
+++ RecoBTag/TensorFlow/pfDeepDoubleBvLJetTags_cfi.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+pfDeepDoubleBvLJetTags = cms.EDProducer('DeepDoubleXTFJetTagsProducer',
+  src = cms.InputTag('pfDeepDoubleXTagInfos'),
+  input_names = cms.vstring(
+    'input_1',
+    'input_2',
+    'input_3'
+  ),
+  lp_names = cms.vstring('db_input_batchnorm/keras_learning_phase'),
+  output_names = cms.vstring('ID_pred/Softmax'),
+  batch_eval = cms.bool(False),
+  nThreads = cms.uint32(1),
+  singleThreadPool = cms.string('no_threads'),
+  flavor = cms.string('BvL'),
+  flav_table = cms.PSet(
+    probQCD = cms.vuint32(0),
+    probHbb = cms.vuint32(1)
+  ),
+  graph_path = cms.FileInPath('RecoBTag/Combined/data/DeepDoubleB/V01/constant_graph_PtCut_MassSculptPen.pb')
+)
