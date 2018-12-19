1657e1ebeefcffb03dab9ee30e08b22a8b17c4f0
Cms Build <cmsbuild@cern.ch>
2018-12-20 00:05:49 +0100

    Updated for CMSSW_10_2_X_2018-12-19-2300

--- RecoBTag/FeatureTools/pfDeepDoubleXTagInfos_cfi.py
+++ RecoBTag/FeatureTools/pfDeepDoubleXTagInfos_cfi.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-pfDeepDoubleBTagInfos = cms.EDProducer('DeepDoubleBTagInfoProducer',
+pfDeepDoubleXTagInfos = cms.EDProducer('DeepDoubleXTagInfoProducer',
   shallow_tag_infos = cms.InputTag('pfBoostedDoubleSVAK8TagInfos'),
   jet_radius = cms.double(0.8),
+  min_jet_pt = cms.double(150),
   min_candidate_pt = cms.double(0.95),
   vertices = cms.InputTag('offlinePrimaryVertices'),
   secondary_vertices = cms.InputTag('inclusiveCandidateSecondaryVertices'),
--- RecoBTag/TensorFlow/pfDeepDoubleBvLJetTags_cfi.py
+++ RecoBTag/TensorFlow/pfDeepDoubleBvLJetTags_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-pfDeepDoubleBJetTags = cms.EDProducer('DeepDoubleXTFJetTagsProducer',
+pfDeepDoubleBvLJetTags = cms.EDProducer('DeepDoubleXTFJetTagsProducer',
   input_names = cms.vstring(
     'input_1',
     'input_2',
@@ -11,11 +11,11 @@ pfDeepDoubleBJetTags = cms.EDProducer('DeepDoubleXTFJetTagsProducer',
   batch_eval = cms.bool(False),
   nThreads = cms.uint32(1),
   singleThreadPool = cms.string('no_threads'),
-  flavor = cms.string('B'),
+  flavor = cms.string('BvL'),
   flav_table = cms.PSet(
-    probQ = cms.vuint32(0),
-    probH = cms.vuint32(1)
+    probQCD = cms.vuint32(0),
+    probHbb = cms.vuint32(1)
   ),
-  src = cms.InputTag('pfDeepDoubleXTagInfosNopt'),
-  graph_path = cms.FileInPath('RecoBTag/Combined/data/DeepDoubleB/V01/constant_graph_PtCut_MassSculptPen.pb')
+  src = cms.InputTag('pfDeepDoubleXTagInfos'),
+  graph_path = cms.FileInPath('RecoBTag/Combined/data/DeepDoubleX/94X/V01/DDB.pb')
 )
