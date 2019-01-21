e7fc250ca40ba6cd50ba2fa861fdc544063cdd08
Cms Build <cmsbuild@cern.ch>
2019-01-21 23:55:32 +0100

    Updated for CMSSW_10_4_X_2019-01-21-2300

--- RecoLocalFastTime/FTLRecProducers/mtdTrackingRecHitProducer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-mtdTrackingRecHitProducer = cms.EDProducer('MTDTrackingRecHitProducer',
-  barrelClusters = cms.InputTag('mtdClusters', 'FTLBarrel'),
-  endcapClusters = cms.InputTag('mtdClusters', 'FTLEndcap')
-)
