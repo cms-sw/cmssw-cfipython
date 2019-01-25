7b053cd05e78ea1e331060bbae8e2089bba8b88a
Cms Build <cmsbuild@cern.ch>
2019-01-25 15:54:13 +0100

    Updated for CMSSW_10_5_X_2019-01-25-1100

--- HeterogeneousCore/CUDAServices/NVProfilerService_cfi.py
+++ HeterogeneousCore/CUDAServices/NVProfilerService_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 NVProfilerService = cms.Service('NVProfilerService',
   highlightModules = cms.untracked.vstring(),
-  showModulePrefetching = cms.untracked.bool(False)
+  showModulePrefetching = cms.untracked.bool(False),
+  skipFirstEvent = cms.untracked.bool(False)
 )
