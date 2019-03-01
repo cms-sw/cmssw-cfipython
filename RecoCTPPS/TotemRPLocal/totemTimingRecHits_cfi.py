1ef13f679dc2749b1373efa1d9975d21b9dd4b8c
Cms Build <cmsbuild@cern.ch>
2019-03-01 12:00:05 +0100

    Updated for CMSSW_10_6_X_2019-03-01-1100

--- RecoCTPPS/TotemRPLocal/totemTimingRecHits_cfi.py
+++ RecoCTPPS/TotemRPLocal/totemTimingRecHits_cfi.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 totemTimingRecHits = cms.EDProducer('TotemTimingRecHitProducer',
   digiTag = cms.InputTag('totemTimingRawToDigi', 'TotemTiming'),
-  calibrationFile = cms.FileInPath('RecoCTPPS/TotemRPLocal/data/timing_offsets_ufsd_2018.dec18.cal.json'),
   baselinePoints = cms.int32(8),
   saturationLimit = cms.double(0.85),
   cfdFraction = cms.double(0.3),
