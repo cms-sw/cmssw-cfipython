c17e9feed9d87c39c0db2ec6a58640c123f47f7e
Cms Build <cmsbuild@cern.ch>
2019-03-05 11:58:31 +0100

    Updated for CMSSW_10_2_X_2019-03-05-1100

---
+++ PhysicsTools/PatAlgos/lowPtGsfLinksDefault_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+lowPtGsfLinksDefault = cms.EDProducer('LowPtGSFToPackedCandidateLinker',
+  PFCandidates = cms.InputTag('particleFlow'),
+  packedCandidates = cms.InputTag('packedPFCandidates'),
+  lostTracks = cms.InputTag('lostTracks'),
+  tracks = cms.InputTag('generalTracks'),
+  gsfPreID = cms.InputTag('lowPtGsfElectronSeeds'),
+  gsfTracks = cms.InputTag('lowPtGsfEleGsfTracks')
+)
