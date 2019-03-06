eca8da3e14d8e1e4203184705ee3e541a873d4bc
Cms Build <cmsbuild@cern.ch>
2019-03-07 00:22:12 +0100

    Updated for CMSSW_10_6_X_2019-03-06-2300

--- PhysicsTools/Utilities/pileupFilter_cfi.py
+++ PhysicsTools/Utilities/pileupFilter_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 pileupFilter = cms.EDFilter('PileUpFilter',
   pileupInfoSummaryInputTag = cms.InputTag('PileupSummaryInfo'),
   minPU = cms.double(0),
-  maxPU = cms.double(80)
+  maxPU = cms.double(80),
+  useTrueNumInteraction = cms.untracked.bool(True)
 )
