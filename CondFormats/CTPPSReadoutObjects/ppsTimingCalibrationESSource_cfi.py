540f43d5ec688ac4c5b2f7c217618faad355b933
Cms Build <cmsbuild@cern.ch>
2019-04-02 05:07:16 +0200

    Updated for CMSSW_10_6_X_2019-04-01-2300

--- CondFormats/CTPPSReadoutObjects/ppsTimingCalibrationESSource_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ppsTimingCalibrationESSource = cms.ESSource('PPSTimingCalibrationESSource',
-  calibrationFile = cms.FileInPath(''),
-  subDetector = cms.uint32(0),
-  appendToDataLabel = cms.string('')
-)
