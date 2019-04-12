1440a973be91139b2bdea83ff925771291c49e85
Cms Build <cmsbuild@cern.ch>
2019-04-12 21:45:33 +0200

    Updated for CMSSW_10_6_X_2019-04-12-1100

--- RecoCTPPS/TotemRPLocal/ctppsDiamondLocalTracks_cfi.py
+++ RecoCTPPS/TotemRPLocal/ctppsDiamondLocalTracks_cfi.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 ctppsDiamondLocalTracks = cms.EDProducer('CTPPSDiamondLocalTrackFitter',
   recHitsTag = cms.InputTag('ctppsDiamondRecHits'),
-  verbosity = cms.int32(0),
   trackingAlgorithmParams = cms.PSet(
     threshold = cms.double(1.5),
     thresholdFromMaximum = cms.double(0.5),
@@ -10,8 +9,10 @@ ctppsDiamondLocalTracks = cms.EDProducer('CTPPSDiamondLocalTrackFitter',
     sigma = cms.double(0.1),
     startFromX = cms.double(-0.5),
     stopAtX = cms.double(19.5),
+    tolerance = cms.double(0.1),
     pixelEfficiencyFunction = cms.string('(x>[0]-0.5*[1])*(x<[0]+0.5*[1])+0*[2]'),
     yPosition = cms.double(0),
-    yWidth = cms.double(0)
+    yWidth = cms.double(0),
+    excludeSingleEdgeHits = cms.bool(True)
   )
 )
