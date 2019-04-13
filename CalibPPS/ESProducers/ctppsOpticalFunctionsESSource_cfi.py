35ed9a8f2022cb988e46976c134ea410128e2529
Cms Build <cmsbuild@cern.ch>
2019-04-13 11:56:23 +0200

    Updated for CMSSW_10_6_X_2019-04-12-2300

--- CalibPPS/ESProducers/ctppsOpticalFunctionsESSource_cfi.py
+++ CalibPPS/ESProducers/ctppsOpticalFunctionsESSource_cfi.py
@@ -1,10 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 ctppsOpticalFunctionsESSource = cms.ESSource('CTPPSOpticalFunctionsESSource',
-  validityRange = cms.EventRange('0:18446744073709551615-0:18446744073709551615'),
-  opticalFunctions = cms.VPSet(
-  ),
-  scoringPlanes = cms.VPSet(
+  configuration = cms.VPSet(
   ),
   appendToDataLabel = cms.string('')
 )
--- RecoCTPPS/ProtonReconstruction/ctppsProtons_cfi.py
+++ RecoCTPPS/ProtonReconstruction/ctppsProtons_cfi.py
@@ -8,6 +8,31 @@ ctppsProtons = cms.EDProducer('CTPPSProtonProducer',
   doMultiRPReconstruction = cms.bool(True),
   singleRPReconstructionLabel = cms.string('singleRP'),
   multiRPReconstructionLabel = cms.string('multiRP'),
+  localAngleXMin = cms.double(-0.03),
+  localAngleXMax = cms.double(0.03),
+  localAngleYMin = cms.double(-0.04),
+  localAngleYMax = cms.double(0.04),
+  association_cuts_45 = cms.PSet(
+    x_cut_apply = cms.bool(False),
+    x_cut_value = cms.double(0.0008),
+    y_cut_apply = cms.bool(False),
+    y_cut_value = cms.double(0.0006),
+    xi_cut_apply = cms.bool(True),
+    xi_cut_value = cms.double(0.013),
+    th_y_cut_apply = cms.bool(True),
+    th_y_cut_value = cms.double(2e-05)
+  ),
+  association_cuts_56 = cms.PSet(
+    x_cut_apply = cms.bool(False),
+    x_cut_value = cms.double(0.0008),
+    y_cut_apply = cms.bool(False),
+    y_cut_value = cms.double(0.0006),
+    xi_cut_apply = cms.bool(True),
+    xi_cut_value = cms.double(0.013),
+    th_y_cut_apply = cms.bool(True),
+    th_y_cut_value = cms.double(2e-05)
+  ),
+  max_n_timing_tracks = cms.uint32(5),
   fitVtxY = cms.bool(True),
   useImprovedInitialEstimate = cms.bool(True)
 )
