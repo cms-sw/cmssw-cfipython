c58ea23f3cbb7bbeec7d2f7b8c2ecb517c74ce26
Cms Build <cmsbuild@cern.ch>
2019-05-01 00:58:14 +0200

    Updated for CMSSW_10_6_X_2019-04-30-2300

--- HLTrigger/special/hltMuonPointingFilter_cfi.py
+++ HLTrigger/special/hltMuonPointingFilter_cfi.py
@@ -4,5 +4,8 @@ hltMuonPointingFilter = cms.EDFilter('HLTMuonPointingFilter',
   SALabel = cms.InputTag('hltCosmicMuonBarrelOnly'),
   PropagatorName = cms.string('SteppingHelixPropagatorAny'),
   radius = cms.double(90),
-  maxZ = cms.double(280)
+  maxZ = cms.double(280),
+  PixHits = cms.uint32(0),
+  TkLayers = cms.uint32(0),
+  MuonHits = cms.uint32(0)
 )
