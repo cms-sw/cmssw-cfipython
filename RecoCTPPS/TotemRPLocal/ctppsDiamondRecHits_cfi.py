486d24754d1b7b0e05e60fa190251faadbc06bce
Cms Build <cmsbuild@cern.ch>
2019-05-02 05:40:19 +0200

    Updated for CMSSW_10_6_X_2019-05-01-2300

--- RecoCTPPS/TotemRPLocal/ctppsDiamondRecHits_cfi.py
+++ RecoCTPPS/TotemRPLocal/ctppsDiamondRecHits_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 ctppsDiamondRecHits = cms.EDProducer('CTPPSDiamondRecHitProducer',
   digiTag = cms.InputTag('ctppsDiamondRawToDigi', 'TimingDiamond'),
+  timingCalibrationTag = cms.string('GlobalTag:PPSDiamondTimingCalibration'),
   timeSliceNs = cms.double(0.0244140625),
   timeShift = cms.int32(0)
 )
--- RecoCTPPS/TotemRPLocal/totemTimingRecHits_cfi.py
+++ RecoCTPPS/TotemRPLocal/totemTimingRecHits_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 totemTimingRecHits = cms.EDProducer('TotemTimingRecHitProducer',
   digiTag = cms.InputTag('totemTimingRawToDigi', 'TotemTiming'),
+  timingCalibrationTag = cms.string('GlobalTag:TotemTimingCalibration'),
   baselinePoints = cms.int32(8),
   saturationLimit = cms.double(0.85),
   cfdFraction = cms.double(0.3),
