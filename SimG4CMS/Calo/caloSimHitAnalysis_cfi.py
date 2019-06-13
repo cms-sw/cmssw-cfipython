c19947e7d4029db4c49a0f453ec032d94cd5b5f0
Cms Build <cmsbuild@cern.ch>
2019-06-13 04:54:29 +0200

    Updated for CMSSW_11_0_X_2019-06-12-2300

---
+++ SimG4CMS/Calo/caloSimHitAnalysis_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+caloSimHitAnalysis = cms.EDAnalyzer('CaloSimHitAnalysis',
+  moduleLabel = cms.untracked.string('g4SimHits'),
+  hitCollection = cms.vstring(
+    'EcalHitsEB1',
+    'EcalHitsEE1',
+    'HcalHits1'
+  ),
+  timeSliceUnit = cms.vdouble(
+    1,
+    1,
+    1
+  ),
+  maxEnergy = cms.untracked.double(200),
+  maxTime = cms.untracked.double(1000),
+  timeCut = cms.untracked.double(100),
+  timeScale = cms.untracked.double(1),
+  testNumbering = cms.untracked.bool(False)
+)
--- Validation/HGCalValidation/hgcalRecHitStudyEE_cfi.py
+++ Validation/HGCalValidation/hgcalRecHitStudyEE_cfi.py
@@ -1,8 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 hgcalRecHitStudyEE = cms.EDAnalyzer('HGCalRecHitStudy',
-  DetectorName = cms.string('HGCalEESensitive'),
-  RecHitSource = cms.InputTag('HGCalRecHit', 'HGCEERecHits'),
-  ifHCAL = cms.bool(False),
-  Verbosity = cms.untracked.int32(0)
+  detectorName = cms.string('HGCalEESensitive'),
+  source = cms.InputTag('HGCalRecHit', 'HGCEERecHits'),
+  ifNose = cms.untracked.bool(False),
+  ifHCAL = cms.untracked.bool(False),
+  verbosity = cms.untracked.int32(0),
+  nBinR = cms.untracked.int32(300),
+  nBinZ = cms.untracked.int32(300),
+  nBinEta = cms.untracked.int32(200),
+  rMin = cms.untracked.double(0),
+  rMax = cms.untracked.double(300),
+  zMin = cms.untracked.double(300),
+  zMax = cms.untracked.double(600),
+  etaMin = cms.untracked.double(1),
+  etaMax = cms.untracked.double(3)
 )
