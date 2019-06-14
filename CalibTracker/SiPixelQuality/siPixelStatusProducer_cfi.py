07c17be0edb3f72512d49305e7cb88a0287b7378
Cms Build <cmsbuild@cern.ch>
2019-06-14 03:33:27 +0200

    Updated for CMSSW_10_6_X_2019-06-13-2300

---
+++ CalibTracker/SiPixelQuality/siPixelStatusProducer_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+siPixelStatusProducer = cms.EDProducer('SiPixelStatusProducer',
+  SiPixelStatusProducerParameters = cms.PSet(
+    resetEveryNLumi = cms.untracked.int32(1),
+    pixelClusterLabel = cms.untracked.InputTag('siPixelClusters', '', 'RECO'),
+    badPixelFEDChannelCollections = cms.VInputTag('siPixelDigis')
+  )
+)
