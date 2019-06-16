d70b4f72378784fdfdf9481723598f124ab99040
Cms Build <cmsbuild@cern.ch>
2019-06-17 01:03:28 +0200

    Updated for CMSSW_11_0_X_2019-06-16-2300

--- RecoEgamma/PhotonIdentification/photonIDValueMapProducer_cfi.py
+++ RecoEgamma/PhotonIdentification/photonIDValueMapProducer_cfi.py
@@ -2,16 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 photonIDValueMapProducer = cms.EDProducer('PhotonIDValueMapProducer',
   particleBasedIsolation = cms.InputTag('particleBasedIsolation', 'gedPhotons'),
-  src = cms.InputTag('gedPhotons'),
-  srcMiniAOD = cms.InputTag('slimmedPhotons', '', '@skipCurrentProcess'),
-  esReducedRecHitCollectionMiniAOD = cms.InputTag('reducedEgamma', 'reducedESRecHits'),
-  eeReducedRecHitCollection = cms.InputTag('reducedEcalRecHitsEE'),
-  pfCandidates = cms.InputTag('particleFlow'),
-  vertices = cms.InputTag('offlinePrimaryVertices'),
-  ebReducedRecHitCollectionMiniAOD = cms.InputTag('reducedEgamma', 'reducedEBRecHits'),
-  eeReducedRecHitCollectionMiniAOD = cms.InputTag('reducedEgamma', 'reducedEERecHits'),
-  esReducedRecHitCollection = cms.InputTag('reducedEcalRecHitsES'),
-  pfCandidatesMiniAOD = cms.InputTag('packedPFCandidates'),
-  verticesMiniAOD = cms.InputTag('offlineSlimmedPrimaryVertices'),
-  ebReducedRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
+  src = cms.InputTag('slimmedPhotons', '', '@skipCurrentProcess'),
+  esReducedRecHitCollection = cms.InputTag('reducedEgamma', 'reducedESRecHits'),
+  ebReducedRecHitCollection = cms.InputTag('reducedEgamma', 'reducedEBRecHits'),
+  eeReducedRecHitCollection = cms.InputTag('reducedEgamma', 'reducedEERecHits'),
+  pfCandidates = cms.InputTag('packedPFCandidates'),
+  vertices = cms.InputTag('offlineSlimmedPrimaryVertices'),
+  isAOD = cms.bool(False)
 )
