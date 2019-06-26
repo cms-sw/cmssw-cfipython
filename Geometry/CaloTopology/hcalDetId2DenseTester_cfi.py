e3dd99bd7a16f8fdb72876d9fd2e926968ee2803
Cms Build <cmsbuild@cern.ch>
2019-06-26 19:33:25 +0200

    Updated for CMSSW_11_0_X_2019-06-26-1100

--- Geometry/CaloTopology/hcalDetId2DenseTester_cfi.py
+++ Geometry/CaloTopology/hcalDetId2DenseTester_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 hcalDetId2DenseTester = cms.EDAnalyzer('HcalDetId2DenseTester',
   fileName = cms.untracked.string(''),
+  testCalib = cms.untracked.bool(False),
   mightGet = cms.optional.untracked.vstring
 )
