67a1db69df53c49ca566a7ba09a9c62ec3e05859
Cms Build <cmsbuild@cern.ch>
2019-07-18 14:26:55 +0200

    Updated for CMSSW_11_0_X_2019-07-17-2300

--- DQM/GEM/GEMDQMStatusDigi_cfi.py
+++ DQM/GEM/GEMDQMStatusDigi_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 GEMDQMStatusDigi = cms.EDProducer('GEMDQMStatusDigi',
   VFATInputLabel = cms.InputTag('muonGEMDigis', 'vfatStatus'),
-  GEBInputLabel = cms.InputTag('muonGEMDigis', 'GEBStatus'),
+  GEBInputLabel = cms.InputTag('muonGEMDigis', 'gebStatus'),
   AMCInputLabel = cms.InputTag('muonGEMDigis', 'AMCStatus'),
   mightGet = cms.optional.untracked.vstring
 )
