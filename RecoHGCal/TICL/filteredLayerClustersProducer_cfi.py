ca08679f5a676950b0333273ea9db78ee855adfc
Cms Build <cmsbuild@cern.ch>
2019-08-08 12:58:45 +0200

    Updated for CMSSW_11_0_X_2019-08-08-1100

--- RecoHGCal/TICL/filteredLayerClustersProducer_cfi.py
+++ RecoHGCal/TICL/filteredLayerClustersProducer_cfi.py
@@ -4,8 +4,9 @@ filteredLayerClustersProducer = cms.EDProducer('FilteredLayerClustersProducer',
   HGCLayerClusters = cms.InputTag('hgcalLayerClusters'),
   LayerClustersInputMask = cms.InputTag('hgcalLayerClusters', 'InitialLayerClustersMask'),
   iteration_label = cms.string('iterationLabelGoesHere'),
-  clusterFilter = cms.string('ClusterFilterByAlgo'),
+  clusterFilter = cms.string('ClusterFilterByAlgoAndSize'),
   algo_number = cms.int32(9),
+  min_cluster_size = cms.int32(0),
   max_cluster_size = cms.int32(9999),
   mightGet = cms.optional.untracked.vstring
 )
