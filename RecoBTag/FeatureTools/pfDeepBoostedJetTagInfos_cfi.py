f758f2e2b1749abc34b5badc7e557458b6c2d933
Cms Build <cmsbuild@cern.ch>
2019-10-03 00:58:40 +0200

    Updated for CMSSW_11_0_X_2019-10-02-2300

--- RecoBTag/FeatureTools/pfDeepBoostedJetTagInfos_cfi.py
+++ RecoBTag/FeatureTools/pfDeepBoostedJetTagInfos_cfi.py
@@ -1,12 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 pfDeepBoostedJetTagInfos = cms.EDProducer('DeepBoostedJetTagInfoProducer',
-  has_puppi_weighted_daughters = cms.bool(True),
   jet_radius = cms.double(0.8),
   min_jet_pt = cms.double(150),
   min_pt_for_track_properties = cms.double(-1),
   vertices = cms.InputTag('offlinePrimaryVertices'),
   secondary_vertices = cms.InputTag('inclusiveCandidateSecondaryVertices'),
+  pf_candidates = cms.InputTag('particleFlow'),
   jets = cms.InputTag('ak8PFJetsPuppi'),
   puppi_value_map = cms.InputTag('puppi'),
   vertex_associator = cms.InputTag('primaryVertexAssociation', 'original'),
