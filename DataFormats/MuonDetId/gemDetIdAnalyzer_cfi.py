5610832a4db1e09f329dc1ca46c39de455349054
Cms Build <cmsbuild@cern.ch>
2019-10-13 18:19:41 +0200

    Updated for CMSSW_11_0_X_2019-10-13-0000

--- DataFormats/MuonDetId/gemDetIdAnalyzer_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-gemDetIdAnalyzer = cms.EDAnalyzer('GEMDetIdAnalyzer',
-  gemInputLabel = cms.InputTag('gemRecHits'),
-  me0InputLabel = cms.InputTag('me0RecHits'),
-  newGEMDetID = cms.bool(True),
-  mightGet = cms.optional.untracked.vstring
-)
