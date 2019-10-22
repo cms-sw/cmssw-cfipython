f8b73f1809e3f6479214a926b3ca385158c8aa38
Cms Build <cmsbuild@cern.ch>
2019-10-23 00:07:48 +0200

    Updated for CMSSW_10_2_X_2019-10-22-2300

---
+++ MuonAnalysis/MuonAssociators/muonFSRProducer_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+muonFSRProducer = cms.EDProducer('MuonFSRProducer',
+  packedPFCandidates = cms.InputTag('packedPFCandidates'),
+  slimmedElectrons = cms.InputTag('slimmedElectrons'),
+  muons = cms.InputTag('slimmedMuons'),
+  muonPtMin = cms.double(20),
+  muonEtaMax = cms.double(2.4),
+  photonPtMin = cms.double(2),
+  deltaROverEt2Max = cms.double(0.05),
+  isolation = cms.double(2)
+)
--- PhysicsTools/NanoAOD/genWeightsTable_cfi.py
+++ PhysicsTools/NanoAOD/genWeightsTable_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 genWeightsTable = cms.EDProducer('GenWeightsTableProducer',
   genEvent = cms.InputTag('generator'),
+  genLumiInfoHeader = cms.InputTag('generator'),
   lheInfo = cms.VInputTag(
     'externalLHEProducer',
     'source'
