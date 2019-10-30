3c50479baa960482a9eb514de3cf44426805148b
Cms Build <cmsbuild@cern.ch>
2019-10-30 04:18:18 +0100

    Updated for CMSSW_11_0_X_2019-10-29-2300

--- Validation/HGCalValidation/hgcalSimHitStudy_cfi.py
+++ Validation/HGCalValidation/hgcalSimHitStudy_cfi.py
@@ -20,7 +20,9 @@ hgcalSimHitStudy = cms.EDAnalyzer('HGCalSimHitStudy',
   nBinR = cms.untracked.int32(300),
   nBinZ = cms.untracked.int32(300),
   nBinEta = cms.untracked.int32(200),
+  layers = cms.untracked.int32(50),
   verbosity = cms.untracked.int32(0),
   ifNose = cms.untracked.bool(False),
+  ifLayer = cms.untracked.bool(False),
   mightGet = cms.optional.untracked.vstring
 )
