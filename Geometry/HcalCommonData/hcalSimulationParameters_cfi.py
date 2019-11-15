c2bbb7ac596fc24b68066b6d7f16ffb799d4edee
Cms Build <cmsbuild@cern.ch>
2019-11-15 04:48:16 +0100

    Updated for CMSSW_11_0_X_2019-11-14-2300

--- Geometry/HcalCommonData/hcalSimulationParameters_cfi.py
+++ Geometry/HcalCommonData/hcalSimulationParameters_cfi.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 hcalSimulationParameters = cms.ESProducer('HcalSimParametersESModule',
+  fromDD4Hep = cms.bool(False),
   appendToDataLabel = cms.string('')
 )
