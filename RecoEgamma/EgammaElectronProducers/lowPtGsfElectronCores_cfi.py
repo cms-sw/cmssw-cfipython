ae48b7f9667706772ca5770eab1bf75bf4ae3240
Cms Build <cmsbuild@cern.ch>
2020-01-30 12:10:09 +0100

    Updated for CMSSW_11_1_X_2020-01-30-1100

--- RecoEgamma/EgammaElectronProducers/lowPtGsfElectronCores_cfi.py
+++ RecoEgamma/EgammaElectronProducers/lowPtGsfElectronCores_cfi.py
@@ -2,9 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 lowPtGsfElectronCores = cms.EDProducer('LowPtGsfElectronCoreProducer',
   gsfPfRecTracks = cms.InputTag('lowPtGsfElePfGsfTracks'),
-  gsfTracks = cms.InputTag('lowPtGsfEleGsfTracks'),
   ctfTracks = cms.InputTag('generalTracks'),
-  useGsfPfRecTracks = cms.bool(True),
   superClusters = cms.InputTag('lowPtGsfElectronSuperClusters'),
   mightGet = cms.optional.untracked.vstring
 )
