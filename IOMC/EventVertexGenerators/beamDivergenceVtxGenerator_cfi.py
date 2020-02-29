476c08a11c8cac6ebb0614678223dbcafb0b69b0
Cms Build <cmsbuild@cern.ch>
2020-02-29 11:54:06 +0100

    Updated for CMSSW_10_6_X_2020-02-29-1100

--- IOMC/EventVertexGenerators/beamDivergenceVtxGenerator_cfi.py
+++ IOMC/EventVertexGenerators/beamDivergenceVtxGenerator_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 beamDivergenceVtxGenerator = cms.EDProducer('BeamDivergenceVtxGenerator',
   src = cms.InputTag('generator', 'unsmeared'),
+  srcGenParticle = cms.VInputTag(),
   simulateBeamDivergence = cms.bool(True),
   simulateVertex = cms.bool(True)
 )
