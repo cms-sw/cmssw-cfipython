a05dc50b407b06ef19b32c4b42e08dce93aef226
Cms Build <cmsbuild@cern.ch>
2020-03-30 14:40:12 +0200

    Updated for CMSSW_10_2_X_2020-03-30-1100

--- PhysicsTools/PatAlgos/muonsUpdated_cfi.py
+++ PhysicsTools/PatAlgos/muonsUpdated_cfi.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 muonsUpdated = cms.EDProducer('PATMuonUpdater',
+  beamspot = cms.InputTag('offlineBeamSpot'),
   computeMiniIso = cms.bool(False),
+  fixDxySign = cms.bool(False),
   pfCandsForMiniIso = cms.InputTag('packedPFCandidates'),
   recomputeMuonBasicSelectors = cms.bool(False)
 )
