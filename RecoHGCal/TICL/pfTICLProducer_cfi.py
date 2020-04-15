396c1b7bc4b19106e2d0bff71866ddb26a722764
Cms Build <cmsbuild@cern.ch>
2020-04-15 03:34:21 +0200

    Updated for CMSSW_11_1_X_2020-04-14-2300

--- RecoHGCal/TICL/pfTICLProducer_cfi.py
+++ RecoHGCal/TICL/pfTICLProducer_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-pfTICL = cms.EDProducer('PFTICLProducer',
+pfTICLProducer = cms.EDProducer('PFTICLProducer',
   ticlCandidateSrc = cms.InputTag('ticlCandidateFromTracksters'),
   mightGet = cms.optional.untracked.vstring
 )
