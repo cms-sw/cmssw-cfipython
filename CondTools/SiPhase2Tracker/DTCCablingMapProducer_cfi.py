3d57af210466eda2def68dc8aec5642762f7e6fe
Cms Build <cmsbuild@cern.ch>
2020-04-20 11:48:00 +0200

    Updated for CMSSW_11_1_X_2020-04-20-1100

--- CondTools/SiPhase2Tracker/DTCCablingMapProducer_cfi.py
+++ CondTools/SiPhase2Tracker/DTCCablingMapProducer_cfi.py
@@ -1,13 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 DTCCablingMapProducer = cms.EDAnalyzer('DTCCablingMapProducer',
-  generate_fake_valid_gbtlink_and_elinkid = cms.bool(False),
+  dummy_fill_mode = cms.string('DUMMY_FILL_DISABLED'),
   verbosity = cms.int32(0),
-  csvFormat_idetid = cms.uint32(0),
   csvFormat_ncolumns = cms.uint32(15),
-  csvFormat_idtcid = cms.uint32(10),
-  csvFormat_igbtlinkid = cms.uint32(11),
-  csvFormat_ielinkid = cms.uint32(12),
+  csvFormat_idetid = cms.uint32(0),
+  csvFormat_idtcid = cms.uint32(0),
+  csvFormat_igbtlinkid = cms.uint32(0),
+  csvFormat_ielinkid = cms.uint32(0),
   iovBeginTime = cms.uint64(1),
   record = cms.string('TrackerDTCCablingMapRcd'),
   modulesToDTCCablingCSVFileNames = cms.vstring(),
