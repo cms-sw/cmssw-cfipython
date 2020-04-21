30021e2bf446d46fcddd2dd65ecce6669b31cb02
Cms Build <cmsbuild@cern.ch>
2020-04-21 12:09:48 +0200

    Updated for CMSSW_11_1_X_2020-04-21-1100

--- HeterogeneousCore/SonicCore/sonicDummyProducerAsync_cfi.py
+++ HeterogeneousCore/SonicCore/sonicDummyProducerAsync_cfi.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 sonicDummyProducerAsync = cms.EDProducer('SonicDummyProducerAsync',
   Client = cms.PSet(
     factor = cms.int32(-1),
-    wait = cms.int32(10)
+    wait = cms.int32(10),
+    allowedTries = cms.uint32(0),
+    fails = cms.uint32(0)
   ),
   input = cms.required.int32,
   mightGet = cms.optional.untracked.vstring
