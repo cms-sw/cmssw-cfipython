1fb9180da84622840a40b30a3c3256a886b185ed
Cms Build <cmsbuild@cern.ch>
2020-05-01 15:31:08 +0200

    Updated for CMSSW_11_1_X_2020-05-01-1100

--- Geometry/EcalAlgo/ecalBarrelCellParameterDump_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ecalBarrelCellParameterDump = cms.EDAnalyzer('ecalBarrelCellParameterDump',
-  SubDetector = cms.int32(1),
-  mightGet = cms.optional.untracked.vstring
-)
--- Geometry/EcalAlgo/ecalEndcapCellParameterDump_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ecalEndcapCellParameterDump = cms.EDAnalyzer('ecalEndcapCellParameterDump',
-  SubDetector = cms.int32(1),
-  mightGet = cms.optional.untracked.vstring
-)
