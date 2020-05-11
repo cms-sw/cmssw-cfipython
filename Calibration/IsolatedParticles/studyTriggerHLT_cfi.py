0a36b78dfc4b7023f21037b20d42970ad7b9a5e0
Cms Build <cmsbuild@cern.ch>
2020-05-11 11:56:52 +0200

    Updated for CMSSW_11_1_X_2020-05-11-1100

--- Calibration/IsolatedParticles/studyTriggerHLT_cfi.py
+++ Calibration/IsolatedParticles/studyTriggerHLT_cfi.py
@@ -1,6 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 studyTriggerHLT = cms.EDAnalyzer('StudyTriggerHLT',
-  verbosity = cms.untracked.int32(0),
+  verbosity = cms.int32(0),
+  labelMuon = cms.InputTag('muons', '', 'RECO'),
+  labelTrack = cms.InputTag('generalTracks', '', 'RECO'),
+  trackQuality = cms.string('highPurity'),
   mightGet = cms.optional.untracked.vstring
 )
