46cd53462e2d09a3215791356b985ab3b06bc1a3
Cms Build <cmsbuild@cern.ch>
2020-05-13 04:45:06 +0200

    Updated for CMSSW_11_1_X_2020-05-12-2300

--- Geometry/MTDNumberingBuilder/mtdNumberingGeometryDB_cfi.py
+++ Geometry/MTDNumberingBuilder/mtdNumberingGeometryDB_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 mtdNumberingGeometryDB = cms.ESProducer('MTDGeometricTimingDetESModule',
   fromDDD = cms.bool(False),
+  fromDD4hep = cms.bool(False),
   appendToDataLabel = cms.string('')
 )
--- Geometry/MTDNumberingBuilder/mtdNumberingGeometry_cfi.py
+++ Geometry/MTDNumberingBuilder/mtdNumberingGeometry_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 mtdNumberingGeometry = cms.ESProducer('MTDGeometricTimingDetESModule',
   fromDDD = cms.bool(True),
+  fromDD4hep = cms.bool(False),
   appendToDataLabel = cms.string('')
 )
