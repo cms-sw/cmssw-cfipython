213e19704421bb87cdd5a9d726814610fc70050f
Cms Build <cmsbuild@cern.ch>
2020-05-13 12:08:52 +0200

    Updated for CMSSW_11_1_X_2020-05-13-1100

--- Validation/HGCalValidation/ticlPFValidationDefault_cfi.py
+++ Validation/HGCalValidation/ticlPFValidationDefault_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 ticlPFValidationDefault = cms.EDProducer('TICLPFValidation',
   folder = cms.string('HGCAL/'),
-  ticlPFCandidates = cms.InputTag('pfTICLProducer'),
+  ticlPFCandidates = cms.InputTag('pfTICL'),
   mightGet = cms.optional.untracked.vstring
 )
