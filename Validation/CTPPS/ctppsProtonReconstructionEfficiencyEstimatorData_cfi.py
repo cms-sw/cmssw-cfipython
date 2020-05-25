fb7fface27e856760cc39f71c8ba5f249ab49833
Cms Build <cmsbuild@cern.ch>
2020-05-26 00:04:37 +0200

    Updated for CMSSW_10_6_X_2020-05-25-2300

---
+++ Validation/CTPPS/ctppsProtonReconstructionEfficiencyEstimatorData_cfi.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+ctppsProtonReconstructionEfficiencyEstimatorData = cms.EDAnalyzer('CTPPSProtonReconstructionEfficiencyEstimatorData',
+  tagTracks = cms.InputTag(''),
+  tagRecoProtonsMultiRP = cms.InputTag(''),
+  pixelDiscardBXShiftedTracks = cms.bool(False),
+  localAngleXMin = cms.double(-0.03),
+  localAngleXMax = cms.double(0.03),
+  localAngleYMin = cms.double(-0.04),
+  localAngleYMax = cms.double(0.04),
+  opticsLabel = cms.string(''),
+  n_prep_events = cms.uint32(1000),
+  n_exp_prot_max = cms.uint32(5),
+  n_sigmas = cms.vdouble(
+    3,
+    5,
+    7
+  ),
+  rpId_45_N = cms.uint32(0),
+  rpId_45_F = cms.uint32(0),
+  rpId_56_N = cms.uint32(0),
+  rpId_56_F = cms.uint32(0),
+  outputFile = cms.string('output.root'),
+  verbosity = cms.untracked.uint32(0)
+)
