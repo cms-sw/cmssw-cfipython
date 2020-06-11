0c82bf67deb8b0de41b2d92226e3ce5277bae46b
Cms Build <cmsbuild@cern.ch>
2020-06-12 00:59:23 +0200

    Updated for CMSSW_11_2_X_2020-06-11-2300

--- Geometry/HcalCommonData/caloDDDSimulationConstants_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-caloDDDSimulationConstants = cms.ESProducer('CaloDDDSimulationConstantsESModule',
-  appendToDataLabel = cms.string('')
-)
--- Geometry/HcalCommonData/hcalDDDSimulationConstants_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hcalDDDSimulationConstants = cms.ESProducer('HcalDDDSimulationConstantsESModule',
-  appendToDataLabel = cms.string('')
-)
