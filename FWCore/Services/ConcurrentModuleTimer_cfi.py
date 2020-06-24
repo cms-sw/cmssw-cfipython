628efe1341b8b764140776e607e529e44317b24e
Cms Build <cmsbuild@cern.ch>
2020-06-24 04:19:03 +0200

    Updated for CMSSW_11_2_X_2020-06-23-2300

--- FWCore/Services/ConcurrentModuleTimer_cfi.py
+++ FWCore/Services/ConcurrentModuleTimer_cfi.py
@@ -2,5 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 ConcurrentModuleTimer = cms.Service('ConcurrentModuleTimer',
   modulesToExclude = cms.untracked.vstring(),
-  excludeSource = cms.untracked.bool(False)
+  excludeSource = cms.untracked.bool(False),
+  padding = cms.untracked.uint32(0),
+  trackGlobalBeginRun = cms.untracked.bool(False)
 )
