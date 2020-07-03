36d4731f8f0c7d03c3d42fae0edb88d7767e6757
Cms Build <cmsbuild@cern.ch>
2020-07-04 00:37:54 +0200

    Updated for CMSSW_11_2_X_2020-07-03-2300

---
+++ HeterogeneousCore/SonicTriton/tritonImageProducerPseudoAsync_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+tritonImageProducerPseudoAsync = cms.EDProducer('TritonImageProducerPseudoAsync',
+  Client = cms.PSet(
+    modelName = cms.required.string,
+    modelVersion = cms.int32(-1),
+    batchSize = cms.required.untracked.uint32,
+    address = cms.required.untracked.string,
+    port = cms.required.untracked.uint32,
+    timeout = cms.required.untracked.uint32,
+    verbose = cms.untracked.bool(False),
+    allowedTries = cms.untracked.uint32(0)
+  ),
+  topN = cms.uint32(5),
+  imageList = cms.required.string,
+  mightGet = cms.optional.untracked.vstring
+)
