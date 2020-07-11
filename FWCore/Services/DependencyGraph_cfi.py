2c81f6d19e9407b623c510921c4620afe4ab510d
Cms Build <cmsbuild@cern.ch>
2020-07-11 15:39:57 +0200

    Updated for CMSSW_11_2_X_2020-07-11-1100

--- FWCore/Services/DependencyGraph_cfi.py
+++ FWCore/Services/DependencyGraph_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 DependencyGraph = cms.Service('DependencyGraph',
-  fileName = cms.untracked.string('dependency.gv'),
+  fileName = cms.untracked.string('dependency.dot'),
   highlightModules = cms.untracked.vstring(),
   showPathDependencies = cms.untracked.bool(True)
 )
