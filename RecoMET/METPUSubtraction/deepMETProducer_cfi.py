483f6a628b342bde506479610dfbff771631a9aa
Cms Build <cmsbuild@cern.ch>
2020-07-16 15:58:35 +0200

    Updated for CMSSW_10_6_X_2020-07-16-1100

---
+++ RecoMET/METPUSubtraction/deepMETProducer_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+deepMETProducer = cms.EDProducer('DeepMETProducer',
+  pf_src = cms.InputTag('packedPFCandidates'),
+  ignore_leptons = cms.bool(False),
+  norm_factor = cms.double(50),
+  max_n_pf = cms.uint32(4500),
+  graph_path = cms.string('RecoMET/METPUSubtraction/data/deepmet/deepmet_v1_2018.pb')
+)
