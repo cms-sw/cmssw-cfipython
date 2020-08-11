cb26e66c8799469271147a549d7fc3184d6514c0
Cms Build <cmsbuild@cern.ch>
2020-08-11 12:02:31 +0200

    Updated for CMSSW_11_2_X_2020-08-11-1100

--- HeterogeneousCore/CUDAServices/CUDAMonitoringService_cfi.py
+++ HeterogeneousCore/CUDAServices/CUDAMonitoringService_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 CUDAMonitoringService = cms.Service('CUDAMonitoringService',
   memoryConstruction = cms.untracked.bool(False),
   memoryBeginStream = cms.untracked.bool(True),
+  memoryPerModule = cms.untracked.bool(True),
   memoryPerEvent = cms.untracked.bool(True)
 )
