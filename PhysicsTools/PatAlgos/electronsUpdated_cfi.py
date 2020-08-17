fd0c308b092f46106c773ff92742c73aa4f812b8
Cms Build <cmsbuild@cern.ch>
2020-08-18 00:02:07 +0200

    Updated for CMSSW_10_6_X_2020-08-17-2300

--- PhysicsTools/PatAlgos/electronsUpdated_cfi.py
+++ PhysicsTools/PatAlgos/electronsUpdated_cfi.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 electronsUpdated = cms.EDProducer('PATElectronUpdater',
+  beamspot = cms.InputTag('offlineBeamSpot'),
   computeMiniIso = cms.bool(False),
+  fixDxySign = cms.bool(False),
   pfCandsForMiniIso = cms.InputTag('packedPFCandidates')
 )
--- RecoBTag/ONNXRuntime/boostedJetONNXJetTagsProducer_cfi.py
+++ RecoBTag/ONNXRuntime/boostedJetONNXJetTagsProducer_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-pfDeepBoostedJetTags = cms.EDProducer('DeepBoostedJetONNXJetTagsProducer',
+boostedJetONNXJetTagsProducer = cms.EDProducer('BoostedJetONNXJetTagsProducer',
   src = cms.InputTag('pfDeepBoostedJetTagInfos'),
+  preprocess_json = cms.string(''),
   preprocessParams = cms.PSet(),
   model_path = cms.FileInPath('RecoBTag/Combined/data/DeepBoostedJet/V02/full/resnet.onnx'),
   flav_names = cms.vstring(
