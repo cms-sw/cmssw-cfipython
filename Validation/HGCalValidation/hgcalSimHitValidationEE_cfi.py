93fa9f214939a54b74e98ca88b9df236d97cdb5b
Cms Build <cmsbuild@cern.ch>
2020-08-18 04:19:19 +0200

    Updated for CMSSW_11_2_X_2020-08-17-2300

--- Validation/HGCalValidation/hgcalSimHitValidationEE_cfi.py
+++ Validation/HGCalValidation/hgcalSimHitValidationEE_cfi.py
@@ -9,5 +9,6 @@ hgcalSimHitValidationEE = cms.EDProducer('HGCalSimHitValidation',
   ),
   Verbosity = cms.untracked.int32(0),
   TestNumber = cms.untracked.bool(True),
+  fromDDD = cms.untracked.bool(True),
   mightGet = cms.optional.untracked.vstring
 )
