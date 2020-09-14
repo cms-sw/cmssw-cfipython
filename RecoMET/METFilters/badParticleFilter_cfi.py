81b3eb39869084b71e053597b0148e9a4abb9419
Cms Build <cmsbuild@cern.ch>
2020-09-14 23:58:34 +0200

    Updated for CMSSW_10_6_X_2020-09-14-2300

---
+++ RecoMET/METFilters/badParticleFilter_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+badParticleFilter = cms.EDFilter('BadParticleFilter',
+  innerTrackRelErr = cms.double(1),
+  minDzBestTrack = cms.double(-1),
+  PFCandidates = cms.InputTag('particleFlow'),
+  filterType = cms.string('BadPFMuon'),
+  segmentCompatibility = cms.double(0.3),
+  minMuonPt = cms.double(100),
+  algo = cms.int32(14),
+  taggingMode = cms.bool(False),
+  vtx = cms.InputTag('offlinePrimaryVertices'),
+  minMuonTrackRelErr = cms.double(2),
+  maxDR = cms.double(0.001),
+  muons = cms.InputTag('muons'),
+  minPtDiffRel = cms.double(0)
+)
