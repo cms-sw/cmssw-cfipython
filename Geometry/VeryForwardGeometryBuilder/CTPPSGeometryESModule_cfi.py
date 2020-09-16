7f3252f5df9eaa99fa0f58d94a167d371d6f4820
Cms Build <cmsbuild@cern.ch>
2020-09-16 03:37:53 +0200

    Updated for CMSSW_11_2_X_2020-09-15-2300

--- Geometry/VeryForwardGeometryBuilder/CTPPSGeometryESModule_cfi.py
+++ Geometry/VeryForwardGeometryBuilder/CTPPSGeometryESModule_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-DoodadESSource = cms.ESProducer('CTPPSGeometryESModule',
+CTPPSGeometryESModule = cms.ESProducer('CTPPSGeometryESModule',
   verbosity = cms.untracked.uint32(1),
   compactViewTag = cms.string(''),
+  fromDD4hep = cms.untracked.bool(False),
   appendToDataLabel = cms.string('')
 )
