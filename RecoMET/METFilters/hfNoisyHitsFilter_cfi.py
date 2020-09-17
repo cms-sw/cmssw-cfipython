9e6e9155c2b9ec380e1578aefcfbcefdce818a41
Cms Build <cmsbuild@cern.ch>
2020-09-17 11:56:11 +0200

    Updated for CMSSW_10_6_X_2020-09-17-1100

---
+++ RecoMET/METFilters/hfNoisyHitsFilter_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+hfNoisyHitsFilter = cms.EDFilter('HFNoisyHitsFilter',
+  hfrechits = cms.InputTag('reducedHcalRecHits', 'hfreco'),
+  rechitPtThreshold = cms.double(20),
+  listOfNoises = cms.vstring(
+    'HFLongShort',
+    'HFS8S1Ratio',
+    'HFPET',
+    'HFSignalAsymmetry'
+  ),
+  taggingMode = cms.bool(False),
+  debug = cms.bool(False)
+)
