a9618cb59ea032ac398e564fbab8311d694638a4
Cms Build <cmsbuild@cern.ch>
2020-11-18 22:05:54 +0100

    Updated for CMSSW_11_2_X_2020-11-18-1600

--- RecoTauTag/RecoTau/DeepTau_cfi.py
+++ RecoTauTag/RecoTau/DeepTau_cfi.py
@@ -12,8 +12,19 @@ DeepTau = cms.EDProducer('DeepTauId',
   version = cms.uint32(2),
   debug_level = cms.int32(0),
   disable_dxy_pca = cms.bool(False),
+  is_online = cms.bool(False),
   VSeWP = cms.required.vstring,
   VSmuWP = cms.required.vstring,
   VSjetWP = cms.required.vstring,
+  basicTauDiscriminators = cms.untracked.InputTag('basicTauDiscriminators'),
+  basicTauDiscriminatorsdR03 = cms.untracked.InputTag('basicTauDiscriminatorsdR03'),
+  pfTauTransverseImpactParameters = cms.InputTag('hpsPFTauTransverseImpactParameters'),
+  Prediscriminants = cms.PSet(
+    BooleanOperator = cms.string('and'),
+    decayMode = cms.PSet(
+      cut = cms.required.double,
+      Producer = cms.required.InputTag
+    )
+  ),
   mightGet = cms.optional.untracked.vstring
 )
