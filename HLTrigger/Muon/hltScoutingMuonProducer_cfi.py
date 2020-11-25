36e1b1ddbb3f64136919c775a67e94c186943344
Cms Build <cmsbuild@cern.ch>
2020-11-25 20:08:14 +0100

    Updated for CMSSW_11_2_X_2020-11-25-1100

--- HLTrigger/Muon/hltScoutingMuonProducer_cfi.py
+++ HLTrigger/Muon/hltScoutingMuonProducer_cfi.py
@@ -1,15 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 hltScoutingMuonProducer = cms.EDProducer('HLTScoutingMuonProducer',
-  ChargedCandidates = cms.InputTag('hltIterL3MuonCandidates'),
+  ChargedCandidates = cms.InputTag('hltIterL3MuonCandidatesNoVtx'),
+  displacedvertexCollection = cms.InputTag('hltDisplacedmumuVtxProducer'),
+  InputMuons = cms.InputTag('hltIterL3MuonsNoVtx'),
+  InputLinks = cms.InputTag('hltL3MuonsIterL3LinksNoVtx'),
   Tracks = cms.InputTag('hltPixelTracks'),
-  EcalPFClusterIsoMap = cms.InputTag('hltMuonEcalMFPFClusterIsoForMuons'),
-  HcalPFClusterIsoMap = cms.InputTag('hltMuonHcalRegPFClusterIsoForMuons'),
-  TrackIsoMap = cms.InputTag('hltMuonTkRelIsolationCut0p07Map', 'combinedRelativeIsoDeposits'),
-  displacedvertexCollection = cms.InputTag('hltPixelVertices'),
+  EcalPFClusterIsoMap = cms.InputTag('hltMuonEcalMFPFClusterIsoForMuonsNoVtx'),
+  HcalPFClusterIsoMap = cms.InputTag('hltMuonHcalPFClusterIsoForMuonsNoVtx'),
+  TrackIsoMap = cms.InputTag('hltMuonTkRelIsolationCut0p09MapNoVtx', 'combinedRelativeIsoDeposits'),
   muonPtCut = cms.double(3),
   muonEtaCut = cms.double(2.4),
   minVtxProbCut = cms.double(0.001),
-  InputLinks = cms.InputTag('hltL3MuonsIterL3Links'),
   mightGet = cms.optional.untracked.vstring
 )
