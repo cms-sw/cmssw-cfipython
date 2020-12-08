1a3c8f913d89035e924fa88b8d56c1998882f60f
Cms Build <cmsbuild@cern.ch>
2020-12-08 12:31:14 +0100

    Updated for CMSSW_11_3_X_2020-12-08-1100

---
+++ CalibPPS/ESProducers/ctppsCompositeESSource_cfi.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+ctppsCompositeESSource = cms.ESSource('CTPPSCompositeESSource',
+  compactViewTag = cms.string(''),
+  lhcInfoLabel = cms.string(''),
+  opticsLabel = cms.string(''),
+  seed = cms.uint32(1),
+  generateEveryNEvents = cms.untracked.uint32(1),
+  verbosity = cms.untracked.uint32(0),
+  periods = cms.VPSet(
+  ),
+  appendToDataLabel = cms.string('')
+)
