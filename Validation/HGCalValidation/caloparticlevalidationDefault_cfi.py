3ebd7048ad6dcf06a2b4f4ca46f951dfbb6a4b25
Cms Build <cmsbuild@cern.ch>
2020-12-11 03:51:50 +0100

    Updated for CMSSW_11_3_X_2020-12-10-2300

--- Validation/HGCalValidation/caloparticlevalidationDefault_cfi.py
+++ Validation/HGCalValidation/caloparticlevalidationDefault_cfi.py
@@ -2,22 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 caloparticlevalidationDefault = cms.EDProducer('CaloParticleValidation',
   folder = cms.string('HGCAL/'),
-  particles_to_monitor = cms.vint32(
-    11,
-    -11,
-    13,
-    -13,
-    22,
-    111,
-    211,
-    -211,
-    321,
-    -321
-  ),
-  simVertices = cms.InputTag('g4SimHits'),
-  caloParticles = cms.InputTag('mix', 'MergedCaloTruth'),
   simPFClusters = cms.InputTag('simPFProducer', 'perfect'),
   simPFCandidates = cms.InputTag('simPFProducer'),
-  hitMapTag = cms.InputTag('hgcalRecHitMapProducer'),
   mightGet = cms.optional.untracked.vstring
 )
