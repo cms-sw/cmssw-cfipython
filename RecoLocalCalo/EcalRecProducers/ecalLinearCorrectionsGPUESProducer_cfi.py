15453e5a02c298ba15adfd276108a371fc4f4c0e
Cms Build <cmsbuild@cern.ch>
2021-01-19 03:48:10 +0100

    Updated for CMSSW_11_3_X_2021-01-18-2300

---
+++ RecoLocalCalo/EcalRecProducers/ecalLinearCorrectionsGPUESProducer_cfi.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+ecalLinearCorrectionsGPUESProducer = cms.ESProducer('EcalLinearCorrectionsGPUESProducer',
+  ComponentName = cms.string(''),
+  label = cms.string(''),
+  appendToDataLabel = cms.string('')
+)
