dd54c1ce5d82dadf4c2e2f7ea5a598104c7923ba
Cms Build <cmsbuild@cern.ch>
2021-05-12 04:27:42 +0200

    Updated for CMSSW_12_0_X_2021-05-11-2300

--- RecoPixelVertexing/PixelTrackFitting/pixelTrackSoA_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pixelTrackSoA = cms.EDProducer('PixelTrackSoAFromCUDA',
-  src = cms.InputTag('caHitNtupletCUDA'),
-  mightGet = cms.optional.untracked.vstring
-)
