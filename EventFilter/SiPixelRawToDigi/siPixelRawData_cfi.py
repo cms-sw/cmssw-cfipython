92612dde4903303ea00bc43920d3a115a2b14fd3
Cms Build <cmsbuild@cern.ch>
2021-05-14 04:22:46 +0200

    Updated for CMSSW_12_0_X_2021-05-13-2300

--- EventFilter/SiPixelRawToDigi/siPixelRawData_cfi.py
+++ EventFilter/SiPixelRawToDigi/siPixelRawData_cfi.py
@@ -3,6 +3,5 @@ import FWCore.ParameterSet.Config as cms
 siPixelRawData = cms.EDProducer('SiPixelDigiToRaw',
   InputLabel = cms.required.InputTag,
   UsePhase1 = cms.bool(False),
-  Timing = cms.untracked.bool(False),
   mightGet = cms.optional.untracked.vstring
 )
