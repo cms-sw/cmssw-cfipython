258fb13e2f8a9412ff1e02a7271db24c40e93969
Cms Build <cmsbuild@cern.ch>
2021-05-21 00:20:54 +0200

    Updated for CMSSW_12_0_X_2021-05-20-2300

--- RecoLocalTracker/SiPixelRecHits/PixelCPEFastESProducer_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/PixelCPEFastESProducer_cfi.py
@@ -10,6 +10,49 @@ PixelCPEFastESProducer = cms.ESProducer('PixelCPEFastESProducer',
   lAWidthFPix = cms.double(0),
   doLorentzFromAlignment = cms.bool(False),
   useLAFromDB = cms.bool(True),
+  xerr_barrel_l1 = cms.vdouble(
+    0.00115,
+    0.0012,
+    0.00088
+  ),
+  yerr_barrel_l1 = cms.vdouble(
+    0.00375,
+    0.0023,
+    0.0025,
+    0.0025,
+    0.0023,
+    0.0023,
+    0.0021,
+    0.0021,
+    0.0024
+  ),
+  xerr_barrel_ln = cms.vdouble(
+    0.00115,
+    0.0012,
+    0.00088
+  ),
+  yerr_barrel_ln = cms.vdouble(
+    0.00375,
+    0.0023,
+    0.0025,
+    0.0025,
+    0.0023,
+    0.0023,
+    0.0021,
+    0.0021,
+    0.0024
+  ),
+  xerr_endcap = cms.vdouble(
+    0.002,
+    0.002
+  ),
+  yerr_endcap = cms.vdouble(0.0021),
+  xerr_barrel_l1_def = cms.double(0.0103),
+  yerr_barrel_l1_def = cms.double(0.0021),
+  xerr_barrel_ln_def = cms.double(0.0103),
+  yerr_barrel_ln_def = cms.double(0.0021),
+  xerr_endcap_def = cms.double(0.002),
+  yerr_endcap_def = cms.double(0.00075),
   EdgeClusterErrorX = cms.double(50),
   EdgeClusterErrorY = cms.double(85),
   UseErrorsFromTemplates = cms.bool(True),
--- RecoLocalTracker/SiPixelRecHits/_generic_default_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/_generic_default_cfi.py
@@ -10,6 +10,49 @@ _generic_default = cms.ESProducer('PixelCPEGenericESProducer',
   lAWidthFPix = cms.double(0),
   doLorentzFromAlignment = cms.bool(False),
   useLAFromDB = cms.bool(True),
+  xerr_barrel_l1 = cms.vdouble(
+    0.00115,
+    0.0012,
+    0.00088
+  ),
+  yerr_barrel_l1 = cms.vdouble(
+    0.00375,
+    0.0023,
+    0.0025,
+    0.0025,
+    0.0023,
+    0.0023,
+    0.0021,
+    0.0021,
+    0.0024
+  ),
+  xerr_barrel_ln = cms.vdouble(
+    0.00115,
+    0.0012,
+    0.00088
+  ),
+  yerr_barrel_ln = cms.vdouble(
+    0.00375,
+    0.0023,
+    0.0025,
+    0.0025,
+    0.0023,
+    0.0023,
+    0.0021,
+    0.0021,
+    0.0024
+  ),
+  xerr_endcap = cms.vdouble(
+    0.002,
+    0.002
+  ),
+  yerr_endcap = cms.vdouble(0.0021),
+  xerr_barrel_l1_def = cms.double(0.0103),
+  yerr_barrel_l1_def = cms.double(0.0021),
+  xerr_barrel_ln_def = cms.double(0.0103),
+  yerr_barrel_ln_def = cms.double(0.0021),
+  xerr_endcap_def = cms.double(0.002),
+  yerr_endcap_def = cms.double(0.00075),
   eff_charge_cut_highX = cms.double(1),
   eff_charge_cut_highY = cms.double(1),
   eff_charge_cut_lowX = cms.double(0),
