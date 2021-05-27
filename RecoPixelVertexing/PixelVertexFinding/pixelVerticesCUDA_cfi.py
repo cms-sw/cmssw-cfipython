78d33ee9c907691751ade8d4f5a9f099ecbc6d93
Cms Build <cmsbuild@cern.ch>
2021-05-28 00:29:25 +0200

    Updated for CMSSW_11_3_X_2021-05-27-2300

--- RecoPixelVertexing/PixelVertexFinding/pixelVerticesCUDA_cfi.py
+++ RecoPixelVertexing/PixelVertexFinding/pixelVerticesCUDA_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-pixelVertexCUDA = cms.EDProducer('PixelVertexProducerCUDA',
+pixelVerticesCUDA = cms.EDProducer('PixelVertexProducerCUDA',
   onGPU = cms.bool(True),
   oneKernel = cms.bool(True),
   useDensity = cms.bool(True),
