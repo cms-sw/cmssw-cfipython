e7f5596e20d9f7f77a9cbeceec6d665ebd815e2f
Cms Build <cmsbuild@cern.ch>
2021-05-28 13:10:09 +0200

    Updated for CMSSW_12_0_X_2021-05-28-1100

--- Geometry/VeryForwardGeometryBuilder/CTPPSGeometryESModule_cfi.py
+++ Geometry/VeryForwardGeometryBuilder/CTPPSGeometryESModule_cfi.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 CTPPSGeometryESModule = cms.ESProducer('CTPPSGeometryESModule',
   verbosity = cms.untracked.uint32(1),
   isRun2 = cms.bool(False),
+  dbTag = cms.string(''),
   compactViewTag = cms.string(''),
+  fromPreprocessedDB = cms.untracked.bool(False),
   fromDD4hep = cms.untracked.bool(False),
   appendToDataLabel = cms.string('')
 )
