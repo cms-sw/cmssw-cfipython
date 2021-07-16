50579ff3f3c269ab166d643666cbb299d25a1b4f
Cms Build <cmsbuild@cern.ch>
2021-07-16 17:18:02 +0200

    Updated for CMSSW_12_0_X_2021-07-16-1100

--- SimG4CMS/ShowerLibraryProducer/hfShowerLibaryAnalysis_cfi.py
+++ SimG4CMS/ShowerLibraryProducer/hfShowerLibaryAnalysis_cfi.py
@@ -1,12 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 hfShowerLibaryAnalysis = cms.EDAnalyzer('HFShowerLibraryAnalyzer',
-  FileName = cms.string('HFShowerLibrary_npmt_noatt_eta4_16en_v4.root'),
+  FileName = cms.string('HFShowerLibrary_10000.root'),
   TreeEMID = cms.string('emParticles'),
   TreeHadID = cms.string('hadParticles'),
   BranchEvt = cms.string(''),
   BranchPre = cms.string(''),
   BranchPost = cms.string(''),
   Verbosity = cms.bool(False),
+  EventPerBin = cms.int32(10000),
   mightGet = cms.optional.untracked.vstring
 )
