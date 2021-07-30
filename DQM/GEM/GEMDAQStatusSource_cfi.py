833db10bb9605fff6ae41d08f592c653dfad0f80
Cms Build <cmsbuild@cern.ch>
2021-07-30 04:47:54 +0200

    Updated for CMSSW_12_0_0_pre5_G4VECGEOM

--- DQM/GEM/GEMDAQStatusSource_cfi.py
+++ DQM/GEM/GEMDAQStatusSource_cfi.py
@@ -1,10 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 GEMDAQStatusSource = cms.EDProducer('GEMDAQStatusSource',
-  VFATInputLabel = cms.InputTag('muonGEMDigis', 'VFATStatus'),
-  OHInputLabel = cms.InputTag('muonGEMDigis', 'OHStatus'),
-  AMCInputLabel = cms.InputTag('muonGEMDigis', 'AMCStatus'),
-  AMC13InputLabel = cms.InputTag('muonGEMDigis', 'AMC13Status'),
+  digisInputLabel = cms.InputTag('muonGEMDigis'),
+  VFATInputLabel = cms.InputTag('muonGEMDigis', 'vfatStatus'),
+  GEBInputLabel = cms.InputTag('muonGEMDigis', 'gebStatus'),
+  AMCInputLabel = cms.InputTag('muonGEMDigis', 'AMCdata'),
+  AMC13InputLabel = cms.InputTag('muonGEMDigis', 'AMC13Event'),
   AMCSlots = cms.int32(13),
   logCategory = cms.untracked.string('GEMDAQStatusSource'),
   mightGet = cms.optional.untracked.vstring
--- DQM/GEM/GEMRecHitSource_cfi.py
+++ DQM/GEM/GEMRecHitSource_cfi.py
@@ -2,9 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 GEMRecHitSource = cms.EDProducer('GEMRecHitSource',
   recHitsInputLabel = cms.InputTag('gemRecHits'),
-  idxFirstDigi = cms.int32(0),
+  idxFirstStrip = cms.int32(0),
   ClusterSizeBinNum = cms.int32(9),
-  modeRelVal = cms.bool(False),
+  global_x_bound_min = cms.double(-350),
+  global_x_bound_max = cms.double(350),
+  global_y_bound_min = cms.double(-260),
+  global_y_bound_max = cms.double(260),
   logCategory = cms.untracked.string('GEMRecHitSource'),
   mightGet = cms.optional.untracked.vstring
 )
