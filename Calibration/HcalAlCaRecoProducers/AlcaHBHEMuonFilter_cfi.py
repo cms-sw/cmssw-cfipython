c204c248bdcf8e1baa7ecd99ddab0ec0c26e3abd
Cms Build <cmsbuild@cern.ch>
2021-08-03 05:59:32 +0200

    Updated for CMSSW_12_1_X_2021-08-02-2300

---
+++ Calibration/HcalAlCaRecoProducers/AlcaHBHEMuonFilter_cfi.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+AlcaHBHEMuonFilter = cms.EDFilter('AlCaHBHEMuonFilter',
+  ProcessName = cms.string('HLT'),
+  TriggerResultLabel = cms.InputTag('TriggerResults', '', 'HLT'),
+  MuonLabel = cms.InputTag('muons'),
+  MinimumMuonP = cms.double(10),
+  Triggers = cms.vstring(
+    'HLT_IsoMu',
+    'HLT_Mu'
+  ),
+  PFCut = cms.bool(True),
+  PFIsolationCut = cms.double(0.12),
+  TrackIsolationCut = cms.double(3),
+  CaloIsolationCut = cms.double(5),
+  PreScale = cms.int32(2),
+  mightGet = cms.optional.untracked.vstring
+)
