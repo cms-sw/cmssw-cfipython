eb6fe834db1f001ad393546c2ee202ae83404ba8
Cms Build <cmsbuild@cern.ch>
2021-08-06 00:28:03 +0200

    Updated for CMSSW_11_3_X_2021-08-05-2300

--- DQM/GEM/GEMDigiSource_cfi.py
+++ DQM/GEM/GEMDigiSource_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 GEMDigiSource = cms.EDProducer('GEMDigiSource',
   digisInputLabel = cms.InputTag('muonGEMDigis'),
   logCategory = cms.untracked.string('GEMDigiSource'),
+  modeRelVal = cms.bool(False),
   mightGet = cms.optional.untracked.vstring
 )
--- DQM/GEM/GEMRecHitSource_cfi.py
+++ DQM/GEM/GEMRecHitSource_cfi.py
@@ -2,12 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 GEMRecHitSource = cms.EDProducer('GEMRecHitSource',
   recHitsInputLabel = cms.InputTag('gemRecHits'),
-  idxFirstStrip = cms.int32(0),
+  idxFirstDigi = cms.int32(0),
   ClusterSizeBinNum = cms.int32(9),
-  global_x_bound_min = cms.double(-350),
-  global_x_bound_max = cms.double(350),
-  global_y_bound_min = cms.double(-260),
-  global_y_bound_max = cms.double(260),
+  modeRelVal = cms.bool(False),
   logCategory = cms.untracked.string('GEMRecHitSource'),
   mightGet = cms.optional.untracked.vstring
 )
