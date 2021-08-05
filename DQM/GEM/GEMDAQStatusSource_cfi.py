8727997feeb445b96f1edac216e9b027f1dc70c3
Cms Build <cmsbuild@cern.ch>
2021-08-05 22:20:01 +0200

    Updated for CMSSW_11_3_4

--- DQM/GEM/GEMDAQStatusSource_cfi.py
+++ DQM/GEM/GEMDAQStatusSource_cfi.py
@@ -1,11 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 GEMDAQStatusSource = cms.EDProducer('GEMDAQStatusSource',
-  digisInputLabel = cms.InputTag('muonGEMDigis'),
-  VFATInputLabel = cms.InputTag('muonGEMDigis', 'vfatStatus'),
-  GEBInputLabel = cms.InputTag('muonGEMDigis', 'gebStatus'),
-  AMCInputLabel = cms.InputTag('muonGEMDigis', 'AMCdata'),
-  AMC13InputLabel = cms.InputTag('muonGEMDigis', 'AMC13Event'),
+  VFATInputLabel = cms.InputTag('muonGEMDigis', 'VFATStatus'),
+  OHInputLabel = cms.InputTag('muonGEMDigis', 'OHStatus'),
+  AMCInputLabel = cms.InputTag('muonGEMDigis', 'AMCStatus'),
+  AMC13InputLabel = cms.InputTag('muonGEMDigis', 'AMC13Status'),
   AMCSlots = cms.int32(13),
   logCategory = cms.untracked.string('GEMDAQStatusSource'),
   mightGet = cms.optional.untracked.vstring
