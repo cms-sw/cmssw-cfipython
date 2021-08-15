a48eaec66a845446c0bb1f29c17dff3caf423c28
Cms Build <cmsbuild@cern.ch>
2021-08-16 00:30:01 +0200

    Updated for CMSSW_12_1_X_2021-08-15-2300

--- RecoHGCal/TICL/trackstersFromCaloParticlesProducer_cfi.py
+++ RecoHGCal/TICL/trackstersFromCaloParticlesProducer_cfi.py
@@ -1,14 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-trackstersFromSimClustersProducer = cms.EDProducer('TrackstersFromSimClustersProducer',
+trackstersFromCaloParticlesProducer = cms.EDProducer('TrackstersFromCaloParticlesProducer',
   detector = cms.string('HGCAL'),
   layer_clusters = cms.InputTag('hgcalLayerClusters'),
   time_layerclusters = cms.InputTag('hgcalLayerClusters', 'timeLayerCluster'),
   filtered_mask = cms.InputTag('filteredLayerClustersSimTracksters', 'ticlSimTracksters'),
-  simclusters = cms.InputTag('mix', 'MergedCaloTruth'),
   caloparticles = cms.InputTag('mix', 'MergedCaloTruth'),
-  layerClusterSimClusterAssociator = cms.InputTag('layerClusterSimClusterAssociationProducer'),
-  layerClusterCaloParticleAssociator = cms.InputTag('layerClusterCaloParticleAssociationProducer'),
+  associator = cms.InputTag('layerClusterCaloParticleAssociationProducer'),
   fractionCut = cms.double(0),
   mightGet = cms.optional.untracked.vstring
 )
