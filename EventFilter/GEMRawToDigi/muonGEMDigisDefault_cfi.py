41b0f32120eed63ec53f5ca04eda07e97d951f79
Cms Build <cmsbuild@cern.ch>
2021-08-19 12:10:55 +0200

    Updated for CMSSW_12_1_X_2021-08-19-1100

--- EventFilter/GEMRawToDigi/muonGEMDigisDefault_cfi.py
+++ EventFilter/GEMRawToDigi/muonGEMDigisDefault_cfi.py
@@ -3,7 +3,6 @@ import FWCore.ParameterSet.Config as cms
 muonGEMDigisDefault = cms.EDProducer('GEMRawToDigiModule',
   InputLabel = cms.InputTag('rawDataCollector'),
   useDBEMap = cms.bool(False),
-  unPackStatusDigis = cms.bool(False),
   keepDAQStatus = cms.bool(False),
   readMultiBX = cms.bool(False),
   mightGet = cms.optional.untracked.vstring
