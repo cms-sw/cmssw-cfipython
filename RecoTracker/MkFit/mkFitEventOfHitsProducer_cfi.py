f445dca0853dee9035e9400f52ce836a4d76ecb9
Cms Build <cmsbuild@cern.ch>
2021-08-25 18:35:41 +0200

    Updated for CMSSW_12_1_X_2021-08-25-1100

--- RecoTracker/MkFit/mkFitEventOfHitsProducer_cfi.py
+++ RecoTracker/MkFit/mkFitEventOfHitsProducer_cfi.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 mkFitEventOfHitsProducer = cms.EDProducer('MkFitEventOfHitsProducer',
   pixelHits = cms.InputTag('mkFitSiPixelHits'),
   stripHits = cms.InputTag('mkFitSiStripHits'),
-  useStripStripQualityDB = cms.bool(False),
+  useStripStripQualityDB = cms.bool(True),
   mightGet = cms.optional.untracked.vstring
 )
