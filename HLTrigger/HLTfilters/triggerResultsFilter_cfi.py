ab4a193d305402c3c108c85b3d71a26504316937
Cms Build <cmsbuild@cern.ch>
2021-09-04 00:17:40 +0200

    Updated for CMSSW_12_1_X_2021-09-03-2300

--- HLTrigger/HLTfilters/triggerResultsFilter_cfi.py
+++ HLTrigger/HLTfilters/triggerResultsFilter_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 triggerResultsFilter = cms.EDFilter('TriggerResultsFilter',
   usePathStatus = cms.bool(False),
-  hltResults = cms.InputTag('TriggerResults'),
+  hltResults = cms.InputTag('TriggerResults', '', '@skipCurrentProcess'),
   l1tResults = cms.InputTag('hltGtStage2Digis'),
   l1tIgnoreMaskAndPrescale = cms.bool(False),
   throw = cms.bool(True),
