fc037d6a1019107029679f1bf5bcb31c76c9d291
Cms Build <cmsbuild@cern.ch>
2021-10-08 04:14:05 +0200

    Updated for CMSSW_12_1_X_2021-10-07-2300

---
+++ RecoMuon/TrackerSeedGenerator/tsgForOIDNN_cfi.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+tsgForOIDNN = cms.EDProducer('TSGForOIDNN',
+  src = cms.InputTag('hltL2Muons', 'UpdatedAtVtx'),
+  layersToTry = cms.int32(2),
+  fixedErrorRescaleFactorForHitless = cms.double(2),
+  hitsToTry = cms.int32(1),
+  MeasurementTrackerEvent = cms.InputTag('hltSiStripClusters'),
+  estimator = cms.string('hltESPChi2MeasurementEstimator100'),
+  maxEtaForTOB = cms.double(1.8),
+  minEtaForTEC = cms.double(0.7),
+  debug = cms.untracked.bool(False),
+  maxSeeds = cms.uint32(20),
+  maxHitlessSeeds = cms.uint32(5),
+  maxHitSeeds = cms.uint32(1),
+  propagatorName = cms.string('PropagatorWithMaterialParabolicMf'),
+  maxHitlessSeedsIP = cms.uint32(5),
+  maxHitlessSeedsMuS = cms.uint32(0),
+  maxHitDoubletSeeds = cms.uint32(0),
+  getStrategyFromDNN = cms.bool(False),
+  useRegressor = cms.bool(False),
+  etaSplitForDnn = cms.double(1),
+  dnnMetadataPath = cms.string(''),
+  mightGet = cms.optional.untracked.vstring
+)
