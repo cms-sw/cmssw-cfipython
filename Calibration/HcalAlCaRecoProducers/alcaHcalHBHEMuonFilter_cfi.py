b02a99ddcb526df50ddb6f443faa74d033a86116
Cms Build <cmsbuild@cern.ch>
2021-10-11 06:19:07 +0200

    Updated for CMSSW_12_1_X_2021-10-10-2300

--- Calibration/HcalAlCaRecoProducers/alcaHcalHBHEMuonFilter_cfi.py
+++ Calibration/HcalAlCaRecoProducers/alcaHcalHBHEMuonFilter_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 alcaHcalHBHEMuonFilter = cms.EDFilter('AlCaHcalHBHEMuonFilter',
   prescale = cms.int32(1),
+  minimumMuonP = cms.double(10),
   hbheMuonLabel = cms.InputTag('alcaHcalHBHEMuonProducer', 'hbheMuon'),
   mightGet = cms.optional.untracked.vstring
 )
