79d4d8d60c65abbeaa506d26f9abc70943ee0824
Cms Build <cmsbuild@cern.ch>
2021-10-11 18:03:08 +0200

    Updated for CMSSW_12_1_X_2021-10-11-1100

--- Calibration/HcalAlCaRecoProducers/alcaHEMuonFilter_cfi.py
+++ Calibration/HcalAlCaRecoProducers/alcaHEMuonFilter_cfi.py
@@ -4,7 +4,6 @@ alcaHEMuonFilter = cms.EDFilter('AlCaHEMuonFilter',
   processName = cms.string('HLT'),
   triggerResultLabel = cms.InputTag('TriggerResults', '', 'HLT'),
   muonLabel = cms.InputTag('muons'),
-  minimumMuonP = cms.double(10),
   triggers = cms.vstring(
     'HLT_IsoMu',
     'HLT_Mu'
