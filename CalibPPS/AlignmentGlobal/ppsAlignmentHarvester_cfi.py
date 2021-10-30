7e42b549109cc61e30708df39e08d5f0efd31d47
Cms Build <cmsbuild@cern.ch>
2021-10-30 12:45:03 +0200

    Updated for CMSSW_12_2_X_2021-10-30-1100

--- CalibPPS/AlignmentGlobal/ppsAlignmentHarvester_cfi.py
+++ CalibPPS/AlignmentGlobal/ppsAlignmentHarvester_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 ppsAlignmentHarvester = cms.EDProducer('PPSAlignmentHarvester',
-  folder = cms.string('CalibPPS/Common'),
+  folder = cms.string('AlCaReco/PPSAlignment'),
   sequence = cms.vstring(
     'x_alignment',
     'x_alignment_relative',
