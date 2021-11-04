c0c20674b374aed8cf1146b78b049985baf28196
Cms Build <cmsbuild@cern.ch>
2021-11-04 04:12:56 +0100

    Updated for CMSSW_12_2_X_2021-11-03-2300

--- L1Trigger/L1TMuon/simGmtShowerDigisDef_cfi.py
+++ L1Trigger/L1TMuon/simGmtShowerDigisDef_cfi.py
@@ -4,7 +4,5 @@ simGmtShowerDigisDef = cms.EDProducer('L1TMuonShowerProducer',
   showerInput = cms.InputTag('simEmtfShowers', 'EMTF'),
   bxMin = cms.int32(0),
   bxMax = cms.int32(0),
-  minNominalShowers = cms.uint32(1),
-  minTwoLooseShowers = cms.uint32(0),
   mightGet = cms.optional.untracked.vstring
 )
--- L1Trigger/L1TMuonEndCap/simEmtfShowersDef_cfi.py
+++ L1Trigger/L1TMuonEndCap/simEmtfShowersDef_cfi.py
@@ -2,9 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 simEmtfShowersDef = cms.EDProducer('L1TMuonEndCapShowerProducer',
   enableOneNominalShowers = cms.bool(True),
+  enableOneTightShowers = cms.bool(True),
   enableTwoLooseShowers = cms.bool(False),
   nLooseShowers = cms.uint32(2),
   nNominalShowers = cms.uint32(1),
+  nTightShowers = cms.uint32(1),
   CSCShowerInput = cms.InputTag('simCscTriggerPrimitiveDigis'),
   mightGet = cms.optional.untracked.vstring
 )
