97d5271e159c52167f8dbf82d346e5804e4cc71e
Cms Build <cmsbuild@cern.ch>
2021-12-01 11:57:11 +0100

    Updated for CMSSW_12_0_X_2021-12-01-1100

--- DQM/GEM/GEMDQMHarvester_cfi.py
+++ DQM/GEM/GEMDQMHarvester_cfi.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 GEMDQMHarvester = cms.EDProducer('GEMDQMHarvester',
-  fromFile = cms.string(''),
+  cutErr = cms.double(0.05),
+  cutLowErr = cms.double(0),
+  cutWarn = cms.double(0.05),
   mightGet = cms.optional.untracked.vstring
 )
