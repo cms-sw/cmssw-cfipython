0fd4bbcbd48f70cc5fc6cc7102902bee558ee8e7
Cms Build <cmsbuild@cern.ch>
2021-12-03 00:30:43 +0100

    Updated for CMSSW_12_2_X_2021-12-02-2300

--- CalibTracker/SiPixelESProducers/siPixelFakeLorentzAngleESSource_cfi.py
+++ CalibTracker/SiPixelESProducers/siPixelFakeLorentzAngleESSource_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 siPixelFakeLorentzAngleESSource = cms.ESSource('SiPixelFakeLorentzAngleESSource',
   file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseI/PixelSkimmedGeometry_phase1.txt'),
   topologyInput = cms.FileInPath('Geometry/TrackerCommonData/data/PhaseI/trackerParameters.xml'),
-  label = cms.string(''),
+  appendToDataLabel = cms.string(''),
   bPixLorentzAnglePerTesla = cms.untracked.double(-9999),
   fPixLorentzAnglePerTesla = cms.untracked.double(-9999),
   BPixParameters = cms.VPSet(
@@ -11,6 +11,5 @@ siPixelFakeLorentzAngleESSource = cms.ESSource('SiPixelFakeLorentzAngleESSource'
   FPixParameters = cms.VPSet(
   ),
   ModuleParameters = cms.VPSet(
-  ),
-  appendToDataLabel = cms.string('')
+  )
 )
