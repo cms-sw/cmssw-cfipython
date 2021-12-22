b13b1e5cd79ff96c1ec8395b66cc3d33e5a434a6
Cms Build <cmsbuild@cern.ch>
2021-12-22 02:44:36 +0100

    Updated for CMSSW_12_3_X_2021-12-21-2300

--- CalibTracker/SiPixelLorentzAngle/siPixelLorentzAnglePCLHarvester_cfi.py
+++ CalibTracker/SiPixelLorentzAngle/siPixelLorentzAnglePCLHarvester_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 siPixelLorentzAnglePCLHarvester = cms.EDProducer('SiPixelLorentzAnglePCLHarvester',
   newmodulelist = cms.vstring(),
   dqmDir = cms.string('AlCaReco/SiPixelLorentzAngle'),
-  fitProbCut = cms.double(0.5),
+  fitProbCut = cms.double(0.1),
   record = cms.string('SiPixelLorentzAngleRcd'),
   mightGet = cms.optional.untracked.vstring
 )
