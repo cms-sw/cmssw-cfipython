fdcf48af5e39f5ca35a640c45842a13c2cd4278e
Cms Build <cmsbuild@cern.ch>
2022-01-13 02:34:03 +0100

    Updated for CMSSW_12_3_X_2022-01-12-2300

--- CondTools/SiStrip/rescaleGain2byGain1_cfi.py
+++ CondTools/SiStrip/rescaleGain2byGain1_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 rescaleGain2byGain1 = cms.EDAnalyzer('SiStripApvGainRescaler',
   Record = cms.string('SiStripApvGainRcd'),
+  printDebug = cms.untracked.uint32(1),
   mightGet = cms.optional.untracked.vstring
 )
