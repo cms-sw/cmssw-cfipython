eb582021262e433073eff081543b4ee918f11369
Cms Build <cmsbuild@cern.ch>
2022-01-25 15:16:33 +0100

    Updated for CMSSW_12_3_X_2022-01-25-1100

--- CommonTools/ParticleFlow/pfNoPileUpPacked_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfNoPileUpPacked = cms.EDProducer('PFNoPileUpPacked',
-  candidates = cms.InputTag('packedPFCandidates'),
-  vertexAssociationQuality = cms.int32(7),
-  vertexAssociation = cms.InputTag('packedPrimaryVertexAssociationJME', 'original'),
-  mightGet = cms.optional.untracked.vstring
-)
