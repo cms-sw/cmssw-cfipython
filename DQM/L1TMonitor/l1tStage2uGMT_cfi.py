3003a2fc207dc267f2c7382e80793d18ce6c5b9c
Cms Build <cmsbuild@cern.ch>
2022-02-11 00:29:45 +0100

    Updated for CMSSW_12_3_X_2022-02-10-2300

--- DQM/L1TMonitor/l1tStage2uGMT_cfi.py
+++ DQM/L1TMonitor/l1tStage2uGMT_cfi.py
@@ -5,9 +5,12 @@ l1tStage2uGMT = cms.EDProducer('L1TStage2uGMT',
   bmtfProducer = cms.required.InputTag,
   omtfProducer = cms.required.InputTag,
   emtfProducer = cms.required.InputTag,
+  muonShowerProducer = cms.required.InputTag,
+  emtfShowerProducer = cms.required.InputTag,
   monitorDir = cms.untracked.string(''),
   emulator = cms.untracked.bool(False),
   verbose = cms.untracked.bool(False),
   displacedQuantities = cms.untracked.bool(False),
+  hadronicShowers = cms.untracked.bool(False),
   mightGet = cms.optional.untracked.vstring
 )
