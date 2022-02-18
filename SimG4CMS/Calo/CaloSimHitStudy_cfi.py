377969875c36de5992e9d627d5c539994b262b7b
Cms Build <cmsbuild@cern.ch>
2022-02-18 09:12:52 +0100

    Updated for CMSSW_12_3_X_2022-02-17-2300

--- SimG4CMS/Calo/CaloSimHitStudy_cfi.py
+++ SimG4CMS/Calo/CaloSimHitStudy_cfi.py
@@ -3,10 +3,12 @@ import FWCore.ParameterSet.Config as cms
 CaloSimHitStudy = cms.EDAnalyzer('CaloSimHitStudy',
   SourceLabel = cms.untracked.string('generatorSmeared'),
   ModuleLabel = cms.untracked.string('g4SimHits'),
-  EBCollection = cms.untracked.string('EcalHitsEB'),
-  EECollection = cms.untracked.string('EcalHitsEE'),
-  ESCollection = cms.untracked.string('EcalHitsES'),
-  HCCollection = cms.untracked.string('HcalHits'),
+  CaloCollection = cms.untracked.vstring(
+    'EcalHitsEB',
+    'EcalHitsEE',
+    'EcalHitsES',
+    'HcalHits'
+  ),
   MaxEnergy = cms.untracked.double(200),
   TimeCut = cms.untracked.double(100),
   MIPCut = cms.untracked.double(0.7),
--- SimG4CMS/Calo/EcalSimHitStudy_cfi.py
+++ SimG4CMS/Calo/EcalSimHitStudy_cfi.py
@@ -2,8 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 EcalSimHitStudy = cms.EDAnalyzer('EcalSimHitStudy',
   ModuleLabel = cms.untracked.string('g4SimHits'),
-  EBCollection = cms.untracked.string('EcalHitsEB'),
-  EECollection = cms.untracked.string('EcalHitsEE'),
+  CaloCollection = cms.untracked.vstring(),
   SourceLabel = cms.untracked.string('VtxSmeared'),
   MaxEnergy = cms.untracked.double(200),
   TimeCut = cms.untracked.double(100),
