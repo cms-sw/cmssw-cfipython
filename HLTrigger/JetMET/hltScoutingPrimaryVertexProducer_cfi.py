711be65decfadc49edc7988e8aa07f141dc08f74
Cms Build <cmsbuild@cern.ch>
2022-02-28 02:18:50 +0100

    Updated for CMSSW_12_3_X_2022-02-27-2300

--- HLTrigger/JetMET/hltScoutingPrimaryVertexProducer_cfi.py
+++ HLTrigger/JetMET/hltScoutingPrimaryVertexProducer_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 hltScoutingPrimaryVertexProducer = cms.EDProducer('HLTScoutingPrimaryVertexProducer',
   vertexCollection = cms.InputTag('hltPixelVertices'),
+  mantissaPrecision = cms.int32(10),
   mightGet = cms.optional.untracked.vstring
 )
