0b37433f2a244e6745f55f3f763152459680c767
Cms Build <cmsbuild@cern.ch>
2022-03-08 03:15:00 +0100

    Updated for CMSSW_12_3_X_2022-03-07-2300

--- RecoTracker/MkFit/mkFitOutputConverter_cfi.py
+++ RecoTracker/MkFit/mkFitOutputConverter_cfi.py
@@ -10,5 +10,11 @@ mkFitOutputConverter = cms.EDProducer('MkFitOutputConverter',
   ttrhBuilder = cms.ESInputTag('', 'WithTrackAngle'),
   propagatorAlong = cms.ESInputTag('', 'PropagatorWithMaterial'),
   propagatorOpposite = cms.ESInputTag('', 'PropagatorWithMaterialOpposite'),
+  qualityMaxInvPt = cms.double(100),
+  qualityMinTheta = cms.double(0.01),
+  qualityMaxR = cms.double(120),
+  qualityMaxZ = cms.double(280),
+  qualityMaxPosErr = cms.double(100),
+  qualitySignPt = cms.bool(True),
   mightGet = cms.optional.untracked.vstring
 )
