dab923c497495babe1816b36b4c1a5333bec181f
Cms Build <cmsbuild@cern.ch>
2022-04-22 13:05:06 +0200

    Updated for CMSSW_12_4_X_2022-04-22-0900

--- RecoTracker/MkFit/mkFitIterationConfigESProducer_cfi.py
+++ RecoTracker/MkFit/mkFitIterationConfigESProducer_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 mkFitIterationConfigESProducer = cms.ESProducer('MkFitIterationConfigESProducer',
   ComponentName = cms.required.string,
   config = cms.required.FileInPath,
+  minPt = cms.double(0),
   appendToDataLabel = cms.string('')
 )
