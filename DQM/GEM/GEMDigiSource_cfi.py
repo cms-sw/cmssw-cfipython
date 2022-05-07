7a55081c839ed708cbe6d71a66da7231090c9f4d
Cms Build <cmsbuild@cern.ch>
2022-05-07 02:30:53 +0200

    Updated for CMSSW_12_4_X_2022-05-06-2300

--- DQM/GEM/GEMDigiSource_cfi.py
+++ DQM/GEM/GEMDigiSource_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 GEMDigiSource = cms.EDProducer('GEMDigiSource',
   digisInputLabel = cms.InputTag('muonGEMDigis'),
+  runType = cms.untracked.string('online'),
   logCategory = cms.untracked.string('GEMDigiSource'),
-  modeRelVal = cms.bool(False),
   bxMin = cms.int32(-10),
   bxMax = cms.int32(10),
   mightGet = cms.optional.untracked.vstring
