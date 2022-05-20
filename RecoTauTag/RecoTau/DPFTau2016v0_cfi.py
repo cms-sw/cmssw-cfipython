9aec0ba479ddfd0cb03be28863182a2f6d9caf48
Cms Build <cmsbuild@cern.ch>
2022-05-20 09:21:00 +0200

    Updated for CMSSW_12_4_X_2022-05-20-0100

--- RecoTauTag/RecoTau/DPFTau2016v0_cfi.py
+++ RecoTauTag/RecoTau/DPFTau2016v0_cfi.py
@@ -4,19 +4,17 @@ DPFTau2016v0 = cms.EDProducer('DPFIsolation',
   pfcands = cms.InputTag('packedPFCandidates'),
   taus = cms.InputTag('slimmedTaus'),
   vertices = cms.InputTag('offlineSlimmedPrimaryVertices'),
-  graph_file = cms.string('RecoTauTag/TrainingFiles/data/DPFTauId/DPFIsolation_2017v0_quantized.pb'),
+  graph_file = cms.vstring('RecoTauTag/TrainingFiles/data/DPFTauId/DPFIsolation_2017v0_quantized.pb'),
   version = cms.uint32(0),
   mem_mapped = cms.bool(False),
-  VSallWP = cms.PSet(
-    VVVLoose = cms.string('0'),
-    VVLoose = cms.string('0'),
-    VLoose = cms.string('0'),
-    Loose = cms.string('0'),
-    Medium = cms.string('0'),
-    Tight = cms.string('0'),
-    VTight = cms.string('0'),
-    VVTight = cms.string('0'),
-    VVVTight = cms.string('0')
+  is_online = cms.bool(False),
+  Prediscriminants = cms.PSet(
+    BooleanOperator = cms.string('and'),
+    decayMode = cms.PSet(
+      cut = cms.required.double,
+      Producer = cms.required.InputTag
+    )
   ),
+  VSallWP = cms.vstring('0'),
   mightGet = cms.optional.untracked.vstring
 )
