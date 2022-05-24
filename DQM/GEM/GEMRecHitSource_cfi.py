5b83d282ca798aafd96357097a998b6cc0b1ec24
Cms Build <cmsbuild@cern.ch>
2022-05-24 15:07:49 +0200

    Updated for CMSSW_12_3_4_patch2

--- DQM/GEM/GEMRecHitSource_cfi.py
+++ DQM/GEM/GEMRecHitSource_cfi.py
@@ -2,11 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 GEMRecHitSource = cms.EDProducer('GEMRecHitSource',
   recHitsInputLabel = cms.InputTag('gemRecHits'),
-  runType = cms.untracked.string('online'),
   idxFirstDigi = cms.int32(0),
-  numDivideEtaPartitionInRPhi = cms.int32(10),
   clsMax = cms.int32(10),
   ClusterSizeBinNum = cms.int32(9),
+  modeRelVal = cms.bool(False),
   logCategory = cms.untracked.string('GEMRecHitSource'),
   mightGet = cms.optional.untracked.vstring
 )
