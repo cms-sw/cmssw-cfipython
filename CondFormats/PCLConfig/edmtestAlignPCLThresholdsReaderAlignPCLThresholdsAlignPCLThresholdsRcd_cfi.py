d92dce9363dd1727f247b23e28ec008b5913b29f
Cms Build <cmsbuild@cern.ch>
2022-06-04 12:38:25 +0200

    Updated for CMSSW_12_5_X_2022-06-04-1100

--- CondFormats/PCLConfig/edmtestAlignPCLThresholdsReaderAlignPCLThresholdsAlignPCLThresholdsRcd_cfi.py
+++ CondFormats/PCLConfig/edmtestAlignPCLThresholdsReaderAlignPCLThresholdsAlignPCLThresholdsRcd_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-AlignPCLThresholdsReader = cms.EDAnalyzer('AlignPCLThresholdsReader',
+edmtestAlignPCLThresholdsReaderAlignPCLThresholdsAlignPCLThresholdsRcd = cms.EDAnalyzer('AlignPCLThresholdsLGReader',
   printDebug = cms.untracked.bool(True),
   outputFile = cms.untracked.string(''),
   mightGet = cms.optional.untracked.vstring
