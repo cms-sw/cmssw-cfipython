68ce6e6ceccd2a0640450e4f2e65b4cc6d2bff13
Cms Build <cmsbuild@cern.ch>
2022-06-23 12:16:19 +0200

    Updated for CMSSW_12_3_X_2022-06-23-1100

--- DQM/GEM/gemEffByGEMCSCSegmentClient_cfi.py
+++ DQM/GEM/gemEffByGEMCSCSegmentClient_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 gemEffByGEMCSCSegmentClient = cms.EDProducer('GEMEffByGEMCSCSegmentClient',
-  folder = cms.untracked.string('GEM/Efficiency/GEMCSCSegment'),
+  confidenceLevel = cms.untracked.double(0.683),
   logCategory = cms.untracked.string('GEMEffByGEMCSCSegmentClient'),
+  folder = cms.untracked.string('GEM/Efficiency/GEMCSCSegment'),
   mightGet = cms.optional.untracked.vstring
 )
