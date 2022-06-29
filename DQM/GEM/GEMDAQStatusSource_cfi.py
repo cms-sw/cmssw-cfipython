f997d0d89f748f5c2f6bd126c258e2315b142db2
Cms Build <cmsbuild@cern.ch>
2022-06-29 12:10:25 +0200

    Updated for CMSSW_12_4_X_2022-06-29-1100

--- DQM/GEM/GEMDAQStatusSource_cfi.py
+++ DQM/GEM/GEMDAQStatusSource_cfi.py
@@ -6,7 +6,7 @@ GEMDAQStatusSource = cms.EDProducer('GEMDAQStatusSource',
   AMCInputLabel = cms.InputTag('muonGEMDigis', 'AMCStatus'),
   AMC13InputLabel = cms.InputTag('muonGEMDigis', 'AMC13Status'),
   AMCSlots = cms.int32(13),
-  runType = cms.untracked.string('online'),
+  runType = cms.untracked.string('relval'),
   logCategory = cms.untracked.string('GEMDAQStatusSource'),
   mightGet = cms.optional.untracked.vstring
 )
--- EventFilter/GEMRawToDigi/muonGEMDigisDefault_cfi.py
+++ EventFilter/GEMRawToDigi/muonGEMDigisDefault_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 muonGEMDigisDefault = cms.EDProducer('GEMRawToDigiModule',
   InputLabel = cms.InputTag('rawDataCollector'),
   useDBEMap = cms.bool(False),
-  keepDAQStatus = cms.bool(False),
+  keepDAQStatus = cms.bool(True),
   readMultiBX = cms.bool(False),
   ge21Off = cms.bool(False),
   fedIdStart = cms.uint32(1467),
