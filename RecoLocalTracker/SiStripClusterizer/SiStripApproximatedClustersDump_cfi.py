d26befef3391e01c3dde13d83cd2bcaf29685cb5
Cms Build <cmsbuild@cern.ch>
2022-07-01 01:08:53 +0200

    Updated for CMSSW_12_5_X_2022-06-30-2300

--- RecoLocalTracker/SiStripClusterizer/SiStripApproximatedClustersDump_cfi.py
+++ RecoLocalTracker/SiStripClusterizer/SiStripApproximatedClustersDump_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 SiStripApproximatedClustersDump = cms.EDAnalyzer('SiStripApproximatedClustersDump',
-  approximatedClustersTag = cms.InputTag('SiStripClusters2ApproxClusters'),
+  approxSiStripClustersTag = cms.InputTag('SiStripClusters2ApproxClusters'),
   mightGet = cms.optional.untracked.vstring
 )
