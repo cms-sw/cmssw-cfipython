475753f2b0225b4700092b99f14cd95da18befc1
Cms Build <cmsbuild@cern.ch>
2022-07-10 08:40:11 +0200

    Updated for CMSSW_12_5_X_2022-07-10-0000

--- CalibTracker/SiStripHitEfficiency/siStripHitEfficiencyHarvester_cfi.py
+++ CalibTracker/SiStripHitEfficiency/siStripHitEfficiencyHarvester_cfi.py
@@ -8,8 +8,13 @@ siStripHitEfficiencyHarvester = cms.EDProducer('SiStripHitEfficiencyHarvester',
   Threshold = cms.double(0.1),
   Title = cms.string('Hit Efficiency'),
   nModsMin = cms.int32(5),
+  doStoreOnTree = cms.untracked.bool(False),
   AutoIneffModTagging = cms.untracked.bool(False),
   TkMapMin = cms.untracked.double(0.9),
+  EffPlotMin = cms.untracked.double(0.9),
   ShowRings = cms.untracked.bool(False),
+  ShowEndcapSides = cms.untracked.bool(True),
+  ShowTOB6TEC9 = cms.untracked.bool(False),
+  ShowOnlyGoodModules = cms.untracked.bool(False),
   mightGet = cms.optional.untracked.vstring
 )
