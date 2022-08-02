b57c1b9462aed881900ad3d6b8ae56ee97bb85d0
Cms Build <cmsbuild@cern.ch>
2022-08-03 00:31:02 +0200

    Updated for CMSSW_12_5_X_2022-08-02-2300

--- RecoTracker/MkFit/mkFitSeedConverter_cfi.py
+++ RecoTracker/MkFit/mkFitSeedConverter_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 mkFitSeedConverter = cms.EDProducer('MkFitSeedConverter',
   seeds = cms.InputTag('initialStepSeeds'),
   ttrhBuilder = cms.ESInputTag('', 'WithTrackAngle'),
+  maxNSeeds = cms.uint32(500000),
   mightGet = cms.optional.untracked.vstring
 )
