b5778f3e0457e1314722783b91258d76f11bba5c
Cms Build <cmsbuild@cern.ch>
2022-08-06 01:00:43 +0200

    Updated for CMSSW_12_5_X_2022-08-05-2300

--- RecoLocalTracker/SiStripClusterizer/SiStripClusters2ApproxClusters_cfi.py
+++ RecoLocalTracker/SiStripClusterizer/SiStripClusters2ApproxClusters_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 SiStripClusters2ApproxClusters = cms.EDProducer('SiStripClusters2ApproxClusters',
   inputClusters = cms.InputTag('siStripClusters'),
+  maxSaturatedStrips = cms.uint32(3),
   mightGet = cms.optional.untracked.vstring
 )
