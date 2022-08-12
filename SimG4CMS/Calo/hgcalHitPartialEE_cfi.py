d21c2c6e7073fbeed402cbd8e628cc1b100f403d
Cms Build <cmsbuild@cern.ch>
2022-08-13 00:28:19 +0200

    Updated for CMSSW_12_5_X_2022-08-12-2300

--- SimG4CMS/Calo/hgcalHitPartialEE_cfi.py
+++ SimG4CMS/Calo/hgcalHitPartialEE_cfi.py
@@ -4,5 +4,6 @@ hgcalHitPartialEE = cms.EDAnalyzer('HGCalHitPartial',
   moduleLabel = cms.string('g4SimHits'),
   caloHitSource = cms.string('HGCHitsEE'),
   nameSense = cms.string('HGCalEESensitive'),
+  missingFile = cms.string(''),
   mightGet = cms.optional.untracked.vstring
 )
