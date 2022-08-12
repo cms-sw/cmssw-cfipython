78951950807f8f8ac54f990f2dee38cde8d6f8ab
Cms Build <cmsbuild@cern.ch>
2022-08-12 03:47:56 +0200

    Updated for CMSSW_12_5_X_2022-08-11-2300

--- EventFilter/RPCRawToDigi/RPCAMCRawToDigi_cfi.py
+++ EventFilter/RPCRawToDigi/RPCAMCRawToDigi_cfi.py
@@ -8,7 +8,8 @@ RPCAMCRawToDigi = cms.EDProducer('RPCAMCRawToDigi',
   RPCAMCUnpackerSettings = cms.PSet(
     fillAMCCounters = cms.bool(True),
     bxMin = cms.int32(-2),
-    bxMax = cms.int32(2)
+    bxMax = cms.int32(2),
+    cppfDaqDelay = cms.int32(0)
   ),
   mightGet = cms.optional.untracked.vstring
 )
