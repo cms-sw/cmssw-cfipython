dc79ba521e4a6a36e999c30fb7ecad6facb3362c
Cms Build <cmsbuild@cern.ch>
2022-08-24 00:12:41 +0200

    Updated for CMSSW_12_4_X_2022-08-23-2300

--- Calibration/HcalAlCaRecoProducers/alcaIsolatedBunchSelector_cfi.py
+++ Calibration/HcalAlCaRecoProducers/alcaIsolatedBunchSelector_cfi.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 alcaIsolatedBunchSelector = cms.EDFilter('AlCaIsolatedBunchSelector',
   triggerResultLabel = cms.InputTag('TriggerResults', '', 'HLT'),
   processName = cms.string('HLT'),
-  triggerName = cms.string('HLT_HcalIsolatedBunch'),
+  triggerName = cms.string(''),
   mightGet = cms.optional.untracked.vstring
 )
