a940350207247fc0b94a8647426970f616573346
Cms Build <cmsbuild@cern.ch>
2022-09-08 12:51:25 +0200

    Updated for CMSSW_12_4_X_2022-09-08-1100

--- DQMOffline/Alignment/diMuonVertexMonitor_cfi.py
+++ DQMOffline/Alignment/diMuonVertexMonitor_cfi.py
@@ -4,6 +4,7 @@ diMuonVertexMonitor = cms.EDProducer('DiMuonVertexMonitor',
   muonTracks = cms.InputTag('ALCARECOTkAlDiMuon'),
   vertices = cms.InputTag('offlinePrimaryVertices'),
   FolderName = cms.string('DiMuonVertexMonitor'),
+  decayMotherName = cms.string('Z'),
   useClosestVertex = cms.bool(True),
   maxSVdist = cms.double(50),
   mightGet = cms.optional.untracked.vstring
