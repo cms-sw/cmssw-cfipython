e70736761394f06ceebdb6a418e91a2639c4e534
Cms Build <cmsbuild@cern.ch>
2022-09-13 01:01:24 +0200

    Updated for CMSSW_12_6_X_2022-09-12-2300

--- HLTrigger/JetMET/hltpfJetsMatchedToFilteredCaloJetsProducer_cfi.py
+++ HLTrigger/JetMET/hltpfJetsMatchedToFilteredCaloJetsProducer_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 hltpfJetsMatchedToFilteredCaloJetsProducer = cms.EDProducer('HLTPFJetsMatchedToFilteredCaloJetsProducer',
   src = cms.InputTag('hltPFJets'),
-  triggerJetsFilter = cms.InputTag('hltSingleJet240Regional'),
+  triggerJetsFilter = cms.InputTag('hltCaloJetsFiltered'),
   triggerJetsType = cms.int32(85),
   maxDeltaR = cms.double(0.5),
   mightGet = cms.optional.untracked.vstring
