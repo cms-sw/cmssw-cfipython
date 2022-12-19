37fc882f6f35e437b584165fb642f90aefe5cf84
Cms Build <cmsbuild@cern.ch>
2022-12-20 00:27:17 +0100

    Updated for CMSSW_13_0_X_2022-12-19-2300

--- FWCore/Services/CPU_cfi.py
+++ FWCore/Services/CPU_cfi.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 CPU = cms.Service('CPU',
-  reportCPUProperties = cms.untracked.bool(False)
+  reportCPUProperties = cms.untracked.bool(False),
+  disableJobReportOutput = cms.untracked.bool(False)
 )
