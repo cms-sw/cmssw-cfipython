1b999e301752b6f3ade80d94a3ef0d113838c990
Cms Build <cmsbuild@cern.ch>
2023-01-08 13:39:37 +0100

    Updated for CMSSW_13_0_X_2023-01-08-0000

--- DQM/SiStripMonitorApproximateCluster/siStripMonitorApproximateCluster_cfi.py
+++ DQM/SiStripMonitorApproximateCluster/siStripMonitorApproximateCluster_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 siStripMonitorApproximateCluster = cms.EDProducer('SiStripMonitorApproximateCluster',
   compareClusters = cms.bool(False),
   ApproxClustersProducer = cms.InputTag('hltSiStripClusters2ApproxClusters'),
-  ClustersProducer = cms.InputTag('siStripClusters'),
+  ClustersProducer = cms.InputTag('hltSiStripClusterizerForRawPrime'),
   folder = cms.string('SiStripApproximateClusters'),
   mightGet = cms.optional.untracked.vstring
 )
