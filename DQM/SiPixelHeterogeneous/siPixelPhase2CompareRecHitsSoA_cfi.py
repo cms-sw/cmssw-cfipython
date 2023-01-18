6ecaca8f2eaf49a0f4acec4fa7180ee3c6772cf1
Cms Build <cmsbuild@cern.ch>
2023-01-18 12:46:44 +0100

    Updated for CMSSW_13_0_X_2023-01-18-1100

--- DQM/SiPixelHeterogeneous/siPixelPhase2CompareRecHitsSoA_cfi.py
+++ DQM/SiPixelHeterogeneous/siPixelPhase2CompareRecHitsSoA_cfi.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 siPixelPhase2CompareRecHitsSoA = cms.EDProducer('SiPixelPhase2CompareRecHitsSoA',
-  pixelHitsSrcCPU = cms.InputTag('siPixelRecHitsPreSplittingSoA@cpu'),
+  pixelHitsSrcCPU = cms.InputTag('siPixelRecHitsPreSplittingSoA@Host'),
   pixelHitsSrcGPU = cms.InputTag('siPixelRecHitsPreSplittingSoA@cuda'),
-  topFolderName = cms.string('SiPixelHeterogeneous/PixelRecHitsCompareGPUvsCPU'),
+  topFolderName = cms.string('SiPixelHeterogeneous/PixelRecHitsCompareDevicevsHost'),
   minD2cut = cms.double(0.0001),
   mightGet = cms.optional.untracked.vstring
 )
