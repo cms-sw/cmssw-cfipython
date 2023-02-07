429c2db78ce29fb345da23b6c489bc4576b1c51a
Cms Build <cmsbuild@cern.ch>
2023-02-07 03:46:32 +0100

    Updated for CMSSW_13_0_X_2023-02-06-2300

--- Geometry/HGCalCommonData/hgcalWaferIDTesterHEF_cfi.py
+++ Geometry/HGCalCommonData/hgcalWaferIDTesterHEF_cfi.py
@@ -4,5 +4,6 @@ hgcalWaferIDTesterHEF = cms.EDAnalyzer('HGCalWaferIDTester',
   nameSense = cms.string('HGCalHESiliconSensitive'),
   fileName = cms.string('cellIDHEF.txt'),
   mode = cms.int32(1),
+  shift = cms.int32(0),
   mightGet = cms.optional.untracked.vstring
 )
