cd476f943822f081c49938d4f00618347db1bca0
Cms Build <cmsbuild@cern.ch>
2023-02-18 00:13:42 +0100

    Updated for CMSSW_13_1_X_2023-02-17-2300

--- RecoVertex/BeamSpotProducer/onlineBeamSpotESProducer_cfi.py
+++ RecoVertex/BeamSpotProducer/onlineBeamSpotESProducer_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 onlineBeamSpotESProducer = cms.ESProducer('OnlineBeamSpotESProducer',
   timeThreshold = cms.int32(48),
   sigmaZThreshold = cms.double(2),
+  sigmaXYThreshold = cms.double(4),
   appendToDataLabel = cms.string('')
 )
