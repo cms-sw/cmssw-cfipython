42da0da9b5b50e6531adc674806c442ce332d14e
Cms Build <cmsbuild@cern.ch>
2023-03-20 11:59:42 +0100

    Updated for CMSSW_13_0_X_2023-03-20-1100

--- RecoTauTag/HLTProducers/l1TJetsMatchingRecoCaloJet_cfi.py
+++ RecoTauTag/HLTProducers/l1TJetsMatchingRecoCaloJet_cfi.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 l1TJetsMatchingRecoCaloJet = cms.EDProducer('L1TCaloJetsMatching',
   L1JetTrigger = cms.InputTag('hltL1DiJetVBF'),
   JetSrc = cms.InputTag('hltAK4PFJetsTightIDCorrected'),
+  matchingMode = cms.string('VBF'),
   pt1Min = cms.double(110),
   pt2Min = cms.double(35),
   pt3Min = cms.double(110),
