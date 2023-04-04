eda85b297518037afb261a0c7bd5cf2f6c19ace8
Cms Build <cmsbuild@cern.ch>
2023-04-04 12:41:07 +0200

    Updated for CMSSW_13_1_X_2023-04-04-1100

---
+++ RecoBTag/FeatureTools/ParticleNetFeatureEvaluator_cfi.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+ParticleNetFeatureEvaluator = cms.EDProducer('ParticleNetFeatureEvaluator',
+  jet_radius = cms.double(0.8),
+  min_jet_pt = cms.double(150),
+  max_jet_eta = cms.double(99),
+  min_jet_eta = cms.double(0),
+  min_pt_for_track_properties = cms.double(-1),
+  min_pt_for_pfcandidates = cms.double(-1),
+  min_pt_for_losttrack = cms.double(1),
+  max_dr_for_losttrack = cms.double(0.4),
+  min_pt_for_taus = cms.double(20),
+  max_eta_for_taus = cms.double(2.5),
+  include_neutrals = cms.bool(True),
+  vertices = cms.InputTag('offlineSlimmedPrimaryVertices'),
+  secondary_vertices = cms.InputTag('slimmedSecondaryVertices'),
+  pf_candidates = cms.InputTag('packedPFCandidates'),
+  losttracks = cms.InputTag('lostTracks'),
+  jets = cms.InputTag('slimmedJetsAK8'),
+  muons = cms.InputTag('slimmedMuons'),
+  taus = cms.InputTag('slimmedTaus'),
+  electrons = cms.InputTag('slimmedElectrons'),
+  photons = cms.InputTag('slimmedPhotons'),
+  mightGet = cms.optional.untracked.vstring
+)
