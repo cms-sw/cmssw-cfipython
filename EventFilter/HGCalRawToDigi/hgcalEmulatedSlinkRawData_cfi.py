e8788884447fe47aaa24fa2f6fa7d6d1bf83e873
Cms Build <cmsbuild@cern.ch>
2023-05-26 03:04:58 +0200

    Updated for CMSSW_13_2_X_2023-05-25-2300

---
+++ EventFilter/HGCalRawToDigi/hgcalEmulatedSlinkRawData_cfi.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+hgcalEmulatedSlinkRawData = cms.EDProducer('HGCalSlinkEmulator',
+  slinkParams = cms.PSet(
+    ECONDs = cms.VPSet(
+      cms.PSet(),
+      cms.PSet(),
+      cms.PSet(),
+      cms.PSet(),
+      cms.PSet(),
+      cms.PSet(),
+      cms.PSet()
+    ),
+    boeMarker = cms.uint32(85),
+    eoeMarker = cms.uint32(170),
+    formatVersion = cms.uint32(3),
+    numCaptureBlocks = cms.uint32(1),
+    checkECONDsLimits = cms.bool(True),
+    storeHeaderTrailer = cms.bool(True)
+  ),
+  emulatorType = cms.string('trivial'),
+  fedId = cms.uint32(0),
+  fedHeaderTrailer = cms.bool(False),
+  storeEmulatorInfo = cms.bool(False),
+  mightGet = cms.optional.untracked.vstring
+)
