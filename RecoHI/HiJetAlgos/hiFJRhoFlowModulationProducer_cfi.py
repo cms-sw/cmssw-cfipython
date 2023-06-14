0125ab0e51e7a5b5a31bf3f4a30b3595c5cba05e
Cms Build <cmsbuild@cern.ch>
2023-06-15 00:27:56 +0200

    Updated for CMSSW_13_2_X_2023-06-14-2300

--- RecoHI/HiJetAlgos/hiFJRhoFlowModulationProducer_cfi.py
+++ RecoHI/HiJetAlgos/hiFJRhoFlowModulationProducer_cfi.py
@@ -1,13 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 hiFJRhoFlowModulationProducer = cms.EDProducer('HiFJRhoFlowModulationProducer',
+  minPfCandidatesPerEvent = cms.int32(100),
   doEvtPlane = cms.bool(False),
   EvtPlane = cms.InputTag('hiEvtPlane'),
   doJettyExclusion = cms.bool(False),
+  exclusionRadius = cms.double(0.4),
   doFreePlaneFit = cms.bool(False),
-  doFlatTest = cms.bool(False),
-  jetTag = cms.InputTag('ak4PFJets'),
-  pfCandSource = cms.InputTag('particleFlow'),
+  jetTag = cms.InputTag('ak4PFJetsForFlow'),
+  pfCandSource = cms.InputTag('packedPFCandidates'),
   evtPlaneLevel = cms.int32(0),
+  pfCandidateEtaCut = cms.double(1),
+  pfCandidateMinPtCut = cms.double(0.3),
+  pfCandidateMaxPtCut = cms.double(3),
+  firstFittedVn = cms.int32(2),
+  lastFittedVn = cms.int32(3),
   mightGet = cms.optional.untracked.vstring
 )
