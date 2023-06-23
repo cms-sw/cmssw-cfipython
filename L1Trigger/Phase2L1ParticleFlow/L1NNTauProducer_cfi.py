88eef26105f1ecd2eee8f0f78e5f60cf63ac02b1
Cms Build <cmsbuild@cern.ch>
2023-06-23 03:25:57 +0200

    Updated for CMSSW_13_2_X_2023-06-22-2300

--- L1Trigger/Phase2L1ParticleFlow/L1NNTauProducer_cfi.py
+++ L1Trigger/Phase2L1ParticleFlow/L1NNTauProducer_cfi.py
@@ -7,6 +7,9 @@ L1NNTauProducer = cms.EDProducer('L1NNTauProducer',
   nparticles = cms.int32(10),
   conesize = cms.double(0.4),
   seedpt = cms.double(20),
+  HW = cms.bool(True),
+  emseed = cms.bool(True),
+  debug = cms.bool(False),
   L1PFObjects = cms.InputTag('L1PFProducer', 'l1pfCandidates'),
   mightGet = cms.optional.untracked.vstring
 )
