62b714b77c8418976c9faea47b499636057548b5
Cms Build <cmsbuild@cern.ch>
2023-07-03 12:04:27 +0200

    Updated for CMSSW_13_2_X_2023-07-03-1100

--- RecoPPS/Local/ctppsDiamondRecHits_cfi.py
+++ RecoPPS/Local/ctppsDiamondRecHits_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 ctppsDiamondRecHits = cms.EDProducer('CTPPSDiamondRecHitProducer',
   digiTag = cms.InputTag('ctppsDiamondRawToDigi', 'TimingDiamond'),
-  timingCalibrationTag = cms.string('GlobalTag:PPSDiamondTimingCalibration'),
+  timingCalibrationTag = cms.string(':PPSDiamondTimingCalibration'),
   timeSliceNs = cms.double(0.0244140625),
   applyCalibration = cms.bool(True),
   mightGet = cms.optional.untracked.vstring
