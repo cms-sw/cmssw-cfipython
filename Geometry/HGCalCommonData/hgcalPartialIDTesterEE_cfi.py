f279ff4cb63415e3b082920b3b4c44e193c347b2
Cms Build <cmsbuild@cern.ch>
2023-08-03 00:28:07 +0200

    Updated for CMSSW_13_3_X_2023-08-02-2300

--- Geometry/HGCalCommonData/hgcalPartialIDTesterEE_cfi.py
+++ Geometry/HGCalCommonData/hgcalPartialIDTesterEE_cfi.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hgcalPartialIDTesterEE = cms.EDAnalyzer('HGCalPartialIDTester',
   nameDetector = cms.string('HGCalEESensitive'),
   fileName = cms.string('partialD98.txt'),
+  invert = cms.bool(False),
   debug = cms.bool(True),
   mightGet = cms.optional.untracked.vstring
 )
