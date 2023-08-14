f434da15508dc9b3d8101a6de066998476f17a60
Cms Build <cmsbuild@cern.ch>
2023-08-15 00:38:02 +0200

    Updated for CMSSW_13_2_X_2023-08-14-2300

---
+++ HLTrigger/JetMET/hltL1TMatchedCaloJetsVBFFilter_cfi.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+hltL1TMatchedCaloJetsVBFFilter = cms.EDFilter('HLTL1TMatchedCaloJetsVBFFilter',
+  saveTags = cms.bool(True),
+  src = cms.InputTag('hltJets'),
+  matchJetsByDeltaR = cms.bool(True),
+  maxJetDeltaR = cms.double(0.5),
+  l1tJetRefs = cms.InputTag('hltL1sJetSeed'),
+  algorithm = cms.string('VBF'),
+  minPt1 = cms.double(110),
+  minPt2 = cms.double(35),
+  minPt3 = cms.double(110),
+  minInvMass = cms.double(650),
+  minNJets = cms.int32(0),
+  maxNJets = cms.int32(-1),
+  triggerType = cms.int32(85),
+  mightGet = cms.optional.untracked.vstring
+)
