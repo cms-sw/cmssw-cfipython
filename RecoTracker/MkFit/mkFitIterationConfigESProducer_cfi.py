01e4025c39c5b0d3443b9957973ea0067dc0066c
Cms Build <cmsbuild@cern.ch>
2023-08-17 03:02:50 +0200

    Updated for CMSSW_13_3_X_2023-08-16-2300

--- RecoTracker/MkFit/mkFitIterationConfigESProducer_cfi.py
+++ RecoTracker/MkFit/mkFitIterationConfigESProducer_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 mkFitIterationConfigESProducer = cms.ESProducer('MkFitIterationConfigESProducer',
-  ComponentName = cms.required.string,
-  config = cms.required.FileInPath,
+  ComponentName = cms.string(''),
+  config = cms.FileInPath(''),
   minPt = cms.double(0),
   maxClusterSize = cms.uint32(8),
   appendToDataLabel = cms.string('')
