29c9cbfb92eae3a60be983d155d6cc032d854bb5
Cms Build <cmsbuild@cern.ch>
2023-08-30 04:19:13 +0200

    Updated for CMSSW_13_3_X_2023-08-29-2300

--- CalibPPS/ESProducers/ctppsInterpolatedOpticalFunctionsESSource_cfi.py
+++ CalibPPS/ESProducers/ctppsInterpolatedOpticalFunctionsESSource_cfi.py
@@ -2,6 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 ctppsInterpolatedOpticalFunctionsESSource = cms.ESProducer('CTPPSInterpolatedOpticalFunctionsESSource',
   lhcInfoLabel = cms.string(''),
+  lhcInfoPerFillLabel = cms.string(''),
+  lhcInfoPerLSLabel = cms.string(''),
   opticsLabel = cms.string(''),
+  useNewLHCInfo = cms.bool(False),
   appendToDataLabel = cms.string('')
 )
--- Validation/CTPPS/ctppsLHCInfoPlotter_cfi.py
+++ Validation/CTPPS/ctppsLHCInfoPlotter_cfi.py
@@ -2,6 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 ctppsLHCInfoPlotter = cms.EDAnalyzer('CTPPSLHCInfoPlotter',
   lhcInfoLabel = cms.string(''),
+  lhcInfoPerLSLabel = cms.string(''),
+  lhcInfoPerFillLabel = cms.string(''),
+  useNewLHCInfo = cms.bool(False),
   outputFile = cms.string(''),
   mightGet = cms.optional.untracked.vstring
 )
