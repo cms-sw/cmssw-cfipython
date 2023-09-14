0f99e12261836081d40dc10cecf1cffcf48f930f
Cms Build <cmsbuild@cern.ch>
2023-09-14 16:09:08 +0200

    Updated for CMSSW_13_3_0_pre3

--- RecoMuon/GlobalTrackingTools/muonBeamspotConstraintValueMapProducer_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-muonBeamspotConstraintValueMapProducer = cms.EDProducer('MuonBeamspotConstraintValueMapProducer',
-  src = cms.InputTag('muons'),
-  beamspot = cms.InputTag('offlineBeamSpot'),
-  vertices = cms.InputTag('offlineSlimmedPrimaryVertices'),
-  mightGet = cms.optional.untracked.vstring
-)
