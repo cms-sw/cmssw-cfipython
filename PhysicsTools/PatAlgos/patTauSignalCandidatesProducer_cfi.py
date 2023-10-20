3f9b2fe51d7ff5c663e0993936c587fca2582e01
Cms Build <cmsbuild@cern.ch>
2023-10-20 15:09:58 +0200

    Updated for CMSSW_13_3_0_pre4

--- PhysicsTools/PatAlgos/patTauSignalCandidatesProducer_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-patTauSignalCandidatesProducer = cms.EDProducer('PATTauSignalCandidatesProducer',
-  src = cms.InputTag('slimmedTaus'),
-  storeLostTracks = cms.bool(True),
-  mightGet = cms.optional.untracked.vstring
-)
