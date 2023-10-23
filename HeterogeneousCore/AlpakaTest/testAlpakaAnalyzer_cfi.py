23414693561099ef2483ad90347ae415cc09dffd
Cms Build <cmsbuild@cern.ch>
2023-10-24 00:47:49 +0200

    Updated for CMSSW_13_3_X_2023-10-23-2300

--- HeterogeneousCore/AlpakaTest/testAlpakaAnalyzer_cfi.py
+++ HeterogeneousCore/AlpakaTest/testAlpakaAnalyzer_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 testAlpakaAnalyzer = cms.EDAnalyzer('TestAlpakaAnalyzer',
   source = cms.required.InputTag,
   expectSize = cms.int32(-1),
+  expectBackend = cms.string(''),
   mightGet = cms.optional.untracked.vstring
 )
