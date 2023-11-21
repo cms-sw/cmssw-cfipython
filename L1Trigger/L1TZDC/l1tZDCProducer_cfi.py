8aaf001308c02d2863f74fda9c1a8f4ed6253c12
Cms Build <cmsbuild@cern.ch>
2023-11-21 01:18:45 +0100

    Updated for CMSSW_14_0_X_2023-11-20-2300

--- L1Trigger/L1TZDC/l1tZDCProducer_cfi.py
+++ L1Trigger/L1TZDC/l1tZDCProducer_cfi.py
@@ -1,9 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 l1tZDCProducer = cms.EDProducer('L1TZDCProducer',
-  zdcDigis = cms.InputTag('hcalDigis', 'ZDC'),
+  hcalTPDigis = cms.InputTag('simHcalTriggerPrimitiveDigis'),
   bxFirst = cms.int32(-2),
   bxLast = cms.int32(2),
-  sampleToCenterBX = cms.int32(4),
   mightGet = cms.optional.untracked.vstring
 )
