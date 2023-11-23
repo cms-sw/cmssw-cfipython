acd4698ee257aa0d3c8999640db0e3e80082bbdd
Cms Build <cmsbuild@cern.ch>
2023-11-23 02:44:51 +0100

    Updated for CMSSW_14_0_X_2023-11-22-2300

--- L1Trigger/L1TTrackMatch/l1TrackVertexAssociationProducer_cfi.py
+++ L1Trigger/L1TTrackMatch/l1TrackVertexAssociationProducer_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 l1TrackVertexAssociationProducer = cms.EDProducer('L1TrackVertexAssociationProducer',
+  l1TracksInputTag = cms.InputTag('l1tGTTInputProducer', 'Level1TTTracksConverted'),
   l1SelectedTracksInputTag = cms.InputTag('l1tTrackSelectionProducer', 'Level1TTTracksSelected'),
   l1SelectedTracksEmulationInputTag = cms.InputTag('l1tTrackSelectionProducer', 'Level1TTTracksSelectedEmulation'),
   l1VerticesInputTag = cms.InputTag('l1tVertexFinder', 'L1Vertices'),
@@ -28,6 +29,7 @@ l1TrackVertexAssociationProducer = cms.EDProducer('L1TrackVertexAssociationProdu
   useDisplacedTracksDeltaZOverride = cms.double(-1),
   processSimulatedTracks = cms.bool(True),
   processEmulatedTracks = cms.bool(True),
+  fwNTrackSetsTVA = cms.uint32(94),
   debug = cms.int32(0),
   mightGet = cms.optional.untracked.vstring
 )
