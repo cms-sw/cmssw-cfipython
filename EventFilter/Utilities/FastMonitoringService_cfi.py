84f99a5065c440f9b975c6eef4ccb96f6241d6a6
Cms Build <cmsbuild@cern.ch>
2023-12-01 03:30:16 +0100

    Updated for CMSSW_14_0_X_2023-11-30-2300

--- EventFilter/Utilities/FastMonitoringService_cfi.py
+++ EventFilter/Utilities/FastMonitoringService_cfi.py
@@ -1,8 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 FastMonitoringService = cms.Service('FastMonitoringService',
+  tbbMonitoringMode = cms.untracked.bool(True),
+  tbbConcurrencyTracker = cms.untracked.bool(True),
   sleepTime = cms.untracked.int32(1),
   fastMonIntervals = cms.untracked.uint32(2),
-  filePerFwkStream = cms.untracked.bool(False),
+  filePerFwkStream = cms.untracked.bool(True),
   verbose = cms.untracked.bool(False)
 )
