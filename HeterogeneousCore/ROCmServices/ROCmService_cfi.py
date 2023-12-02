e8434a7e24ac602f6fd0aeec6cad644289e4f6ff
Cms Build <cmsbuild@cern.ch>
2023-12-02 05:10:14 +0100

    Updated for CMSSW_14_0_X_2023-12-01-2300

--- HeterogeneousCore/ROCmServices/ROCmService_cfi.py
+++ HeterogeneousCore/ROCmServices/ROCmService_cfi.py
@@ -5,8 +5,6 @@ ROCmService = cms.Service('ROCmService',
   verbose = cms.untracked.bool(False),
   limits = cms.untracked.PSet(
     hipLimitStackSize = cms.untracked.int32(-1),
-    hipLimitMallocHeapSize = cms.untracked.int32(-1),
-    hipLimitDevRuntimeSyncDepth = cms.untracked.int32(-1),
-    hipLimitDevRuntimePendingLaunchCount = cms.untracked.int32(-1)
+    hipLimitMallocHeapSize = cms.untracked.int32(-1)
   )
 )
