5419408fdc52bfadf8a91afbc557955d762ac782
Cms Build <cmsbuild@cern.ch>
2023-12-05 02:59:44 +0100

    Updated for CMSSW_13_3_X_2023-12-04-2300

--- PhysicsTools/NanoAOD/L1TriggerResultsConverter_cfi.py
+++ PhysicsTools/NanoAOD/L1TriggerResultsConverter_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 L1TriggerResultsConverter = cms.EDProducer('L1TriggerResultsConverter',
   legacyL1 = cms.required.bool,
   src = cms.required.InputTag,
-  storeUnprefireableBit = cms.bool(False),
+  storeUnprefireableBits = cms.bool(False),
   src_ext = cms.InputTag(''),
   mightGet = cms.optional.untracked.vstring
 )
