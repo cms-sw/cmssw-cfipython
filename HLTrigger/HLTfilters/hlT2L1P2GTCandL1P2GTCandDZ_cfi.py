59ef2a96520c3514e58a397013f13d9d26858f59
Cms Build <cmsbuild@cern.ch>
2024-01-24 05:50:17 +0100

    Updated for CMSSW_14_0_X_2024-01-23-2300

---
+++ HLTrigger/HLTfilters/hlT2L1P2GTCandL1P2GTCandDZ_cfi.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+hlT2L1P2GTCandL1P2GTCandDZ = cms.EDFilter('HLT2L1P2GTCandL1P2GTCandDZ',
+  saveTags = cms.bool(True),
+  originTag1 = cms.VInputTag('hltOriginal1'),
+  originTag2 = cms.VInputTag('hltOriginal2'),
+  l1GTAlgoBlockTag = cms.InputTag('l1tGTAlgoBlockProducer'),
+  l1GTAlgoName1 = cms.string(''),
+  l1GTAlgoName2 = cms.string(''),
+  triggerType1 = cms.int32(0),
+  triggerType2 = cms.int32(0),
+  MinDR = cms.double(-1),
+  MaxDZ = cms.double(0.2),
+  MinPixHitsForDZ = cms.int32(0),
+  checkSC = cms.bool(False),
+  MinN = cms.int32(1),
+  mightGet = cms.optional.untracked.vstring
+)
