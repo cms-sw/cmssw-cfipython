2446883805dddbec060afc7750b6d23bce58978e
Cms Build <cmsbuild@cern.ch>
2024-02-15 03:59:09 +0100

    Updated for CMSSW_14_0_X_2024-02-14-2300

--- HeterogeneousCore/AlpakaTest/alpaka_rocm_asyncTestAlpakaProducer_cfi.py
+++ HeterogeneousCore/AlpakaTest/alpaka_rocm_asyncTestAlpakaProducer_cfi.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 alpaka_rocm_asyncTestAlpakaProducer = cms.EDProducer('alpaka_rocm_async::TestAlpakaProducer',
   size = cms.required.int32,
+  size2 = cms.required.int32,
+  size3 = cms.required.int32,
   mightGet = cms.optional.untracked.vstring,
   alpaka = cms.untracked.PSet(
     backend = cms.untracked.string('')
--- HeterogeneousCore/AlpakaTest/testAlpakaProducer_cfi.py
+++ HeterogeneousCore/AlpakaTest/testAlpakaProducer_cfi.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 testAlpakaProducer = cms.EDProducer('TestAlpakaProducer@alpaka',
   size = cms.required.int32,
+  size2 = cms.required.int32,
+  size3 = cms.required.int32,
   mightGet = cms.optional.untracked.vstring,
   alpaka = cms.untracked.PSet(
     backend = cms.untracked.string('')
