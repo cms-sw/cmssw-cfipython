31dc6b4581f4fbc2f69849fa4c17719b5f0692d2
Cms Build <cmsbuild@cern.ch>
2024-02-22 00:51:24 +0100

    Updated for CMSSW_14_0_X_2024-02-21-2300

--- RecoEgamma/EgammaHLTProducers/hltEgammaHLTClusterShapeProducer_cfi.py
+++ RecoEgamma/EgammaHLTProducers/hltEgammaHLTClusterShapeProducer_cfi.py
@@ -4,7 +4,6 @@ hltEgammaHLTClusterShapeProducer = cms.EDProducer('EgammaHLTClusterShapeProducer
   recoEcalCandidateProducer = cms.InputTag('hltL1SeededRecoEcalCandidate'),
   ecalRechitEB = cms.InputTag('hltEcalRegionalEgammaRecHit', 'EcalRecHitsEB'),
   ecalRechitEE = cms.InputTag('hltEcalRegionalEgammaRecHit', 'EcalRecHitsEE'),
-  isIeta = cms.bool(True),
   multThresEB = cms.double(1),
   multThresEE = cms.double(1.25),
   mightGet = cms.optional.untracked.vstring
