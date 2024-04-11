8ee8475df41e1397d4727cb98a59bd0061221cbb
Cms Build <cmsbuild@cern.ch>
2024-04-12 01:15:06 +0200

    Updated for CMSSW_14_1_X_2024-04-11-2300

--- Geometry/HGCalCommonData/HGCalCellOffsetTester.py
+++ Geometry/HGCalCommonData/HGCalCellOffsetTester.py
@@ -2,11 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 def HGCalCellOffsetTester(**kwargs):
   mod = cms.EDAnalyzer('HGCalCellOffsetTester',
-    waferSize = cms.double(166.4408),
+    waferSize = cms.double(167.4408),
     waferType = cms.int32(0),
-    cellPlacementIndex = cms.int32(3),
-    mouseBiteCut = cms.double(0),
-    guardRingOffset = cms.double(0),
+    cellPlacementIndex = cms.int32(11),
+    cellType = cms.int32(22),
+    mouseBiteCut = cms.double(5),
+    guardRingOffset = cms.double(0.9),
     mightGet = cms.optional.untracked.vstring
   )
   for k,v in kwargs.items():
--- Geometry/HGCalCommonData/hgcalCellOffsetTester_cfi.py
+++ Geometry/HGCalCommonData/hgcalCellOffsetTester_cfi.py
@@ -1,10 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 hgcalCellOffsetTester = cms.EDAnalyzer('HGCalCellOffsetTester',
-  waferSize = cms.double(166.4408),
+  waferSize = cms.double(167.4408),
   waferType = cms.int32(0),
-  cellPlacementIndex = cms.int32(3),
-  mouseBiteCut = cms.double(0),
-  guardRingOffset = cms.double(0),
+  cellPlacementIndex = cms.int32(11),
+  cellType = cms.int32(22),
+  mouseBiteCut = cms.double(5),
+  guardRingOffset = cms.double(0.9),
   mightGet = cms.optional.untracked.vstring
 )
