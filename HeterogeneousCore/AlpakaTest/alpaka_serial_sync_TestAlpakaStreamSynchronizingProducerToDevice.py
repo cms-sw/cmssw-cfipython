52720ef55c6dd592506586f9e017bfa03f319217
Cms Build <cmsbuild@cern.ch>
2024-04-30 06:02:28 +0200

    Updated for CMSSW_14_1_X_2024-04-29-2300

---
+++ HeterogeneousCore/AlpakaTest/alpaka_serial_sync_TestAlpakaStreamSynchronizingProducerToDevice.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+def alpaka_serial_sync_TestAlpakaStreamSynchronizingProducerToDevice(**kwargs):
+  mod = cms.EDProducer('alpaka_serial_sync::TestAlpakaStreamSynchronizingProducerToDevice',
+    size = cms.PSet(
+      alpaka_serial_sync = cms.required.int32,
+      alpaka_cuda_async = cms.required.int32,
+      alpaka_rocm_async = cms.required.int32
+    ),
+    mightGet = cms.optional.untracked.vstring,
+    alpaka = cms.untracked.PSet(
+      backend = cms.untracked.string('')
+    )
+  )
+  for k,v in kwargs.items():
+    setattr(mod, k, v)
+  return mod
