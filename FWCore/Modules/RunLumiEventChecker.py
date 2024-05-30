c79c2d94c00e2fd5af3514d0e445c2ec11f0c629
Cms Build <cmsbuild@cern.ch>
2024-05-30 12:42:44 +0200

    Updated for CMSSW_14_1_X_2024-05-30-1100

--- FWCore/Modules/RunLumiEventChecker.py
+++ FWCore/Modules/RunLumiEventChecker.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 def RunLumiEventChecker(**kwargs):
   mod = cms.EDAnalyzer('RunLumiEventChecker',
     eventSequence = cms.required.untracked.VEventID,
+    minNumberOfEvents = cms.untracked.uint32(0),
+    maxNumberOfEvents = cms.untracked.uint32(0),
     unorderedEvents = cms.untracked.bool(False),
     mightGet = cms.optional.untracked.vstring
   )
