eff08a32006d804a94ca1fd0efb76d104a1d88d0
Cms Build <cmsbuild@cern.ch>
2024-07-22 14:42:06 +0200

    Updated for CMSSW_14_0_12_MULTIARCHS

--- HLTrigger/special/hltPixelTrackFilter_cfi.py
+++ HLTrigger/special/hltPixelTrackFilter_cfi.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 hltPixelTrackFilter = cms.EDFilter('HLTPixelTrackFilter',
-  saveTags = cms.bool(True),
   pixelTracks = cms.InputTag('hltPixelTracks'),
   minPixelTracks = cms.uint32(0),
   maxPixelTracks = cms.uint32(0),
