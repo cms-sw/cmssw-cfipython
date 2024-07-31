e3d045f9d3fee54645694729f30318843cf2f243
Cms Build <cmsbuild@cern.ch>
2024-07-31 12:33:38 +0200

    Updated for CMSSW_14_0_X_2024-07-31-1100

--- DQM/SiPixelHeterogeneous/siPixelPhase1CompareRecHitsSoAAlpaka_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-siPixelPhase1CompareRecHitsSoAAlpaka = cms.EDProducer('SiPixelPhase1CompareRecHitsSoAAlpaka',
-  pixelHitsSrcHost = cms.InputTag('siPixelRecHitsPreSplittingAlpakaSerial'),
-  pixelHitsSrcDevice = cms.InputTag('siPixelRecHitsPreSplittingAlpaka'),
-  topFolderName = cms.string('SiPixelHeterogeneous/PixelRecHitsCompareDeviceVSHost'),
-  minD2cut = cms.double(0.0001),
-  mightGet = cms.optional.untracked.vstring
-)
