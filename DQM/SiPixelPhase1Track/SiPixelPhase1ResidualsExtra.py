2fddd993b9bbf8819e792dc1d77e7afda3ded449
Cms Build <cmsbuild@cern.ch>
2024-08-22 06:41:40 +0200

    Updated for CMSSW_14_1_X_2024-08-21-2300

--- DQM/SiPixelPhase1Track/SiPixelPhase1ResidualsExtra.py
+++ DQM/SiPixelPhase1Track/SiPixelPhase1ResidualsExtra.py
@@ -2,6 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 def SiPixelPhase1ResidualsExtra(**kwargs):
   mod = cms.EDProducer('SiPixelPhase1ResidualsExtra',
+    TopFolderName = cms.string('PixelPhase1/Tracks/ResidualsExtra'),
+    InputFolderName = cms.string(''),
+    MinHits = cms.int32(30),
     mightGet = cms.optional.untracked.vstring
   )
   for k,v in kwargs.items():
