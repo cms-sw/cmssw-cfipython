13db20c8bbe714c7870bf1c5c412ca7f93e24bf6
Cms Build <cmsbuild@cern.ch>
2024-08-24 11:37:24 +0200

    Updated for CMSSW_14_1_X_2024-08-23-2300

--- L1Trigger/L1TZDC/L1TZDCAnalyzer.py
+++ L1Trigger/L1TZDC/L1TZDCAnalyzer.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 def L1TZDCAnalyzer(**kwargs):
   mod = cms.EDAnalyzer('L1TZDCAnalyzer',
-    etSumTag = cms.InputTag('etSumZdcProducer'),
+    etSumTag = cms.InputTag('l1tZDCEtSums'),
     mightGet = cms.optional.untracked.vstring
   )
   for k,v in kwargs.items():
