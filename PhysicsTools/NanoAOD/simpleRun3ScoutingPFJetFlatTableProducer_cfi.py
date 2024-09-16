3899964ccaf4d6744f8b6d7f73fa788e0e4e8a35
Cms Build <cmsbuild@cern.ch>
2024-09-17 00:18:13 +0200

    Updated for CMSSW_14_0_X_2024-09-16-2300

---
+++ PhysicsTools/NanoAOD/simpleRun3ScoutingPFJetFlatTableProducer_cfi.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+simpleRun3ScoutingPFJetFlatTableProducer = cms.EDProducer('SimpleRun3ScoutingPFJetFlatTableProducer',
+  name = cms.required.string,
+  doc = cms.string(''),
+  extension = cms.bool(False),
+  skipNonExistingSrc = cms.bool(False),
+  src = cms.required.InputTag,
+  variables = cms.PSet(),
+  singleton = cms.bool(False),
+  cut = cms.string(''),
+  lazyEval = cms.untracked.bool(False),
+  maxLen = cms.optional.uint32,
+  externalVariables = cms.PSet(),
+  mightGet = cms.optional.untracked.vstring
+)
