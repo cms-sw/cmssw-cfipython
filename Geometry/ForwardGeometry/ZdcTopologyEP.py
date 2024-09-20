defc1a6546cca5fb0c3bd03e50ac342e12290fc9
Cms Build <cmsbuild@cern.ch>
2024-09-20 14:16:47 +0200

    Updated for CMSSW_14_1_X_2024-09-20-1100

--- Geometry/ForwardGeometry/ZdcTopologyEP.py
+++ Geometry/ForwardGeometry/ZdcTopologyEP.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 def ZdcTopologyEP(**kwargs):
   mod = cms.ESProducer('ZdcTopologyEP',
-    Exclude = cms.untracked.string(''),
     appendToDataLabel = cms.string('')
   )
   for k,v in kwargs.items():
