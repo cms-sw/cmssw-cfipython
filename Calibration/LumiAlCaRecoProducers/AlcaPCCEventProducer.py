5116fc7c45690158139009070ac482587f23ef0b
Cms Build <cmsbuild@cern.ch>
2024-10-05 00:33:30 +0200

    Updated for CMSSW_14_2_X_2024-10-04-2300

--- Calibration/LumiAlCaRecoProducers/AlcaPCCEventProducer.py
+++ Calibration/LumiAlCaRecoProducers/AlcaPCCEventProducer.py
@@ -4,6 +4,7 @@ def AlcaPCCEventProducer(*args, **kwargs):
   mod = cms.EDProducer('AlcaPCCEventProducer',
     pixelClusterLabel = cms.InputTag('siPixelClustersForLumi'),
     trigstring = cms.untracked.string('alcaPCCEvent'),
+    savePerROCInfo = cms.bool(True),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
