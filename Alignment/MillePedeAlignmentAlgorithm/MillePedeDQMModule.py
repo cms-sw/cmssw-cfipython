7819b6dce9e20075ad98d99ea7ae16a33029cf5b
Cms Build <cmsbuild@cern.ch>
2024-10-11 00:52:02 +0200

    Updated for CMSSW_14_2_X_2024-10-10-2300

--- Alignment/MillePedeAlignmentAlgorithm/MillePedeDQMModule.py
+++ Alignment/MillePedeAlignmentAlgorithm/MillePedeDQMModule.py
@@ -2,6 +2,16 @@ import FWCore.ParameterSet.Config as cms
 
 def MillePedeDQMModule(*args, **kwargs):
   mod = cms.EDProducer('MillePedeDQMModule',
+    outputFolder = cms.string('AlCaReco/SiPixelAli'),
+    MillePedeFileReader = cms.PSet(
+      fileDir = cms.string(''),
+      ignoreInactiveAlignables = cms.bool(True),
+      millePedeEndFile = cms.string('millepede.end'),
+      millePedeLogFile = cms.string('millepede.log'),
+      millePedeResFile = cms.string('millepede.res'),
+      isHG = cms.bool(False)
+    ),
+    alignmentTokenSrc = cms.InputTag('SiPixelAliPedeAlignmentProducer'),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
