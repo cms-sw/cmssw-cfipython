89ecc14db386850ea1476a90f90e0458a31e91c2
Cms Build <cmsbuild@cern.ch>
2024-12-13 00:20:40 +0100

    Updated for CMSSW_14_2_X_2024-12-12-2300

--- SimCalorimetry/HGCalAssociatorProducers/tracksterToSimTracksterAssociatorByHitsProducer_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from .TracksterToSimTracksterAssociatorByHitsProducer import TracksterToSimTracksterAssociatorByHitsProducer
-
-tracksterToSimTracksterAssociatorByHitsProducer = TracksterToSimTracksterAssociatorByHitsProducer()
