f286a76846422f0c33d43369340f279b0fdc0806
Cms Build <cmsbuild@cern.ch>
2025-01-22 03:56:27 +0100

    Updated for CMSSW_14_1_X_2025-01-21-2300

--- HeterogeneousCore/CUDAServices/CUDAService.py
+++ HeterogeneousCore/CUDAServices/CUDAService.py
@@ -8,7 +8,6 @@ def CUDAService(**kwargs):
       cudaLimitPrintfFifoSize = cms.untracked.int32(-1),
       cudaLimitStackSize = cms.untracked.int32(-1),
       cudaLimitMallocHeapSize = cms.untracked.int32(-1),
-      cudaLimitDevRuntimeSyncDepth = cms.untracked.int32(-1),
       cudaLimitDevRuntimePendingLaunchCount = cms.untracked.int32(-1)
     ),
     allocator = cms.untracked.PSet(
