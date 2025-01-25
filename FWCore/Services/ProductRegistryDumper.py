8a4dc178e1ab24f5b1d06543d50aa0447430a5ac
Cms Build <cmsbuild@cern.ch>
2025-01-25 09:18:29 +0100

    Updated for CMSSW_15_0_X_2025-01-24-2300

--- FWCore/Services/ProductRegistryDumper.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-def ProductRegistryDumper(*args, **kwargs):
-  mod = cms.Service('ProductRegistryDumper')
-  for a in args:
-    mod.update_(a)
-  mod.update_(kwargs)
-  return mod
