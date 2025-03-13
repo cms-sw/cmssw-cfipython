9212e0349608c3c9b0c4b992f62e722409069932
Cms Build <cmsbuild@cern.ch>
2025-03-13 03:18:25 +0100

    Updated for CMSSW_15_1_X_2025-03-12-2300

--- RecoLocalTracker/SiStripRecHitConverter/StripCPEESProducer.py
+++ RecoLocalTracker/SiStripRecHitConverter/StripCPEESProducer.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 def StripCPEESProducer(*args, **kwargs):
   mod = cms.ESProducer('StripCPEESProducer',
-    ComponentName = cms.string('SimpleStripCPE'),
+    ComponentName = cms.string('stripCPE'),
     ComponentType = cms.string('SimpleStripCPE'),
     parameters = cms.PSet(),
     appendToDataLabel = cms.string('')
