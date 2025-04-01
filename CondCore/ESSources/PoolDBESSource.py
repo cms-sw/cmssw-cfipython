019f4709f94574fce06559732ce05e5c6d73c8e2
Cms Build <cmsbuild@cern.ch>
2025-04-02 00:04:10 +0200

    Updated for CMSSW_15_0_X_2025-04-01-2300

--- CondCore/ESSources/PoolDBESSource.py
+++ CondCore/ESSources/PoolDBESSource.py
@@ -2,6 +2,27 @@ import FWCore.ParameterSet.Config as cms
 
 def PoolDBESSource(*args, **kwargs):
   mod = cms.ESSource('PoolDBESSource',
+    DBParameters = cms.PSet(
+      authenticationPath = cms.untracked.string(''),
+      authenticationSystem = cms.untracked.int32(0),
+      security = cms.untracked.string(''),
+      messageLevel = cms.untracked.int32(0),
+      connectionTimeout = cms.untracked.int32(0)
+    ),
+    connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+    globaltag = cms.string(''),
+    snapshotTime = cms.string(''),
+    frontierKey = cms.untracked.string(''),
+    toGet = cms.VPSet(
+    ),
+    JsonDumpFileName = cms.untracked.string(''),
+    DumpStat = cms.untracked.bool(False),
+    ReconnectEachRun = cms.untracked.bool(False),
+    RefreshAlways = cms.untracked.bool(False),
+    RefreshEachRun = cms.untracked.bool(False),
+    RefreshOpenIOVs = cms.untracked.bool(False),
+    pfnPostfix = cms.untracked.string(''),
+    pfnPrefix = cms.untracked.string(''),
     appendToDataLabel = cms.string('')
   )
   for a in args:
---
+++ CondCore/ESSources/default_CondDBESource_cfi.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+from .PoolDBESSource import PoolDBESSource
+
+default_CondDBESource = PoolDBESSource()
