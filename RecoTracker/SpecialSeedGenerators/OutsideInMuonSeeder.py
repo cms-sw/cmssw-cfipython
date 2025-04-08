c4589a9acd6a937ac6ac7e82bd20ecbc2fb85d82
Cms Build <cmsbuild@cern.ch>
2025-04-08 05:23:07 +0200

    Updated for CMSSW_15_1_X_2025-04-07-2300

--- RecoTracker/SpecialSeedGenerators/OutsideInMuonSeeder.py
+++ RecoTracker/SpecialSeedGenerators/OutsideInMuonSeeder.py
@@ -2,6 +2,20 @@ import FWCore.ParameterSet.Config as cms
 
 def OutsideInMuonSeeder(*args, **kwargs):
   mod = cms.EDProducer('OutsideInMuonSeeder',
+    src = cms.InputTag('muons'),
+    cut = cms.string(''),
+    layersToTry = cms.int32(3),
+    hitsToTry = cms.int32(3),
+    fromVertex = cms.bool(True),
+    errorRescaleFactor = cms.double(2),
+    trackerPropagator = cms.string(''),
+    muonPropagator = cms.string(''),
+    measurementTkEvent = cms.InputTag('MeasurementTrackerEvent'),
+    hitCollector = cms.string(''),
+    updatorLabel = cms.string('KFUpdator'),
+    minEtaForTEC = cms.double(0.7),
+    maxEtaForTOB = cms.double(1.8),
+    debug = cms.untracked.bool(False),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
