027da42c1dd0f0119a5292d80a51507185f2dfdc
Cms Build <cmsbuild@cern.ch>
2025-06-01 06:47:14 +0200

    Updated for CMSSW_15_1_X_2025-06-01-0000

--- Validation/MtdValidation/BtlSimHitsValidation.py
+++ Validation/MtdValidation/BtlSimHitsValidation.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 def BtlSimHitsValidation(*args, **kwargs):
   mod = cms.EDProducer('BtlSimHitsValidation',
     folder = cms.string('MTD/BTL/SimHits'),
+    optionalPlots = cms.bool(False),
     inputTag = cms.InputTag('mix', 'g4SimHitsFastTimerHitsBarrel'),
     hitMinimumEnergy = cms.double(1),
     mightGet = cms.optional.untracked.vstring
