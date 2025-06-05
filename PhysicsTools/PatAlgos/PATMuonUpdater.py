843d49b9ac033479bee0f6553946df2386ccce1e
Cms Build <cmsbuild@cern.ch>
2025-06-05 06:48:37 +0200

    Updated for CMSSW_15_0_X_2025-06-04-2300

--- PhysicsTools/PatAlgos/PATMuonUpdater.py
+++ PhysicsTools/PatAlgos/PATMuonUpdater.py
@@ -9,6 +9,8 @@ def PATMuonUpdater(*args, **kwargs):
     fixDxySign = cms.bool(False),
     pfCandsForMiniIso = cms.InputTag('packedPFCandidates'),
     recomputeMuonBasicSelectors = cms.bool(False),
+    recomputeSoftMuonMvaRun3 = cms.bool(False),
+    softMvaRun3Model = cms.string('RecoMuon/MuonIdentification/data/Run2022-20231030-1731-Event0'),
     miniIsoParams = cms.optional.vdouble,
     mightGet = cms.optional.untracked.vstring
   )
