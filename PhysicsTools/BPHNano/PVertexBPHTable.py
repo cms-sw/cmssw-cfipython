e50dc0c1f68f685903b9ab9c91640e2b63a0322c
Cms Build <cmsbuild@cern.ch>
2025-07-01 00:31:37 +0200

    Updated for CMSSW_15_1_X_2025-06-30-2300

--- PhysicsTools/BPHNano/PVertexBPHTable.py
+++ PhysicsTools/BPHNano/PVertexBPHTable.py
@@ -2,6 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 def PVertexBPHTable(*args, **kwargs):
   mod = cms.EDProducer('PVertexBPHTable',
+    pvSrc = cms.required.InputTag,
+    dileptons = cms.required.InputTag,
+    maxDzDilep = cms.required.double,
+    goodPvCut = cms.required.string,
+    pvName = cms.required.string,
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
