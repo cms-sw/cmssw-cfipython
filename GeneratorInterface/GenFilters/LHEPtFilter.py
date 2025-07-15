30a8022b8d4d3ce128e8947f639856339722a000
Cms Build <cmsbuild@cern.ch>
2025-07-15 11:53:59 +0200

    Updated for CMSSW_15_1_X_2025-07-15-1100

---
+++ GeneratorInterface/GenFilters/LHEPtFilter.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+def LHEPtFilter(*args, **kwargs):
+  mod = cms.EDFilter('LHEPtFilter',
+    isScalar = cms.bool(False),
+    mightGet = cms.optional.untracked.vstring
+  )
+  for a in args:
+    mod.update_(a)
+  mod.update_(kwargs)
+  return mod
