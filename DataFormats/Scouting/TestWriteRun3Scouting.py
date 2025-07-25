0bbb842149b24224b06b9d073bafa2b1aa241178
Cms Build <cmsbuild@cern.ch>
2025-07-25 11:52:05 +0200

    Updated for CMSSW_15_0_X_2025-07-25-1100

--- DataFormats/Scouting/TestWriteRun3Scouting.py
+++ DataFormats/Scouting/TestWriteRun3Scouting.py
@@ -17,6 +17,12 @@ def TestWriteRun3Scouting(*args, **kwargs):
     tracksIntegralValues = cms.required.vint32,
     vertexesFloatingPointValues = cms.required.vdouble,
     vertexesIntegralValues = cms.required.vint32,
+    ebRecHitsFloatingPointValues = cms.required.vdouble,
+    ebRecHitsIntegralValues = cms.required.vint32,
+    eeRecHitsFloatingPointValues = cms.required.vdouble,
+    eeRecHitsIntegralValues = cms.required.vint32,
+    hbheRecHitsFloatingPointValues = cms.required.vdouble,
+    hbheRecHitsIntegralValues = cms.required.vint32,
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
