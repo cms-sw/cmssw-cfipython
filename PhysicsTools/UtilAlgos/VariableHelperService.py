f5cef79521d7c7f87ce8b435ecbb95abbe39465d
Cms Build <cmsbuild@cern.ch>
2025-07-31 04:05:52 +0200

    Updated for CMSSW_15_1_X_2025-07-30-2300

--- PhysicsTools/UtilAlgos/VariableHelperService.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-def VariableHelperService(*args, **kwargs):
-  mod = cms.Service('VariableHelperService')
-  for a in args:
-    mod.update_(a)
-  mod.update_(kwargs)
-  return mod
