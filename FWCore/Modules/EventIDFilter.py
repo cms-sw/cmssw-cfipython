49db4ee851697b3f49ca6c98d23acf354c7b57f0
Cms Build <cmsbuild@cern.ch>
2025-08-19 03:26:49 +0200

    Updated for CMSSW_15_1_X_2025-08-18-2300

--- FWCore/Modules/EventIDFilter.py
+++ FWCore/Modules/EventIDFilter.py
@@ -1,9 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-def TestFilterModule(*args, **kwargs):
-  mod = cms.EDFilter('TestFilterModule',
-    acceptValue = cms.untracked.int32(1),
-    onlyOne = cms.untracked.bool(False),
+def EventIDFilter(*args, **kwargs):
+  mod = cms.EDFilter('EventIDFilter',
+    eventsToPass = cms.required.VEventID,
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
