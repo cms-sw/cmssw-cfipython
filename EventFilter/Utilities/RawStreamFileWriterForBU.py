907ce58e7d64f2475e3b7866ec5462e27b636d71
Cms Build <cmsbuild@cern.ch>
2025-09-12 11:59:49 +0200

    Updated for CMSSW_15_0_X_2025-09-12-1100

--- EventFilter/Utilities/RawStreamFileWriterForBU.py
+++ EventFilter/Utilities/RawStreamFileWriterForBU.py
@@ -6,8 +6,10 @@ def RawStreamFileWriterForBU(*args, **kwargs):
     numEventsPerFile = cms.uint32(100),
     frdVersion = cms.uint32(6),
     sourceIdList = cms.untracked.vuint32(),
+    writeToOpen = cms.untracked.bool(False),
     microSleep = cms.int32(0),
-    frdFileVersion = cms.uint32(0)
+    frdFileVersion = cms.uint32(0),
+    writeEoR = cms.untracked.bool(True)
   )
   for a in args:
     mod.update_(a)
