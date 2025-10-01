4831fd2e247b7aee54cbdaf0272bbc55afd47086
Cms Build <cmsbuild@cern.ch>
2025-10-01 12:20:24 +0200

    Updated for CMSSW_14_2_X_2025-10-01-1100

--- L1Trigger/L1TCalorimeter/L1TEtSumsPrinter.py
+++ L1Trigger/L1TCalorimeter/L1TEtSumsPrinter.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-def L1TZDCAnalyzer(*args, **kwargs):
-  mod = cms.EDAnalyzer('L1TZDCAnalyzer',
-    etSumTag = cms.InputTag('l1tZDCEtSums'),
+def L1TEtSumsPrinter(*args, **kwargs):
+  mod = cms.EDAnalyzer('L1TEtSumsPrinter',
+    src = cms.InputTag('gtStage2Digis', 'EtSum'),
+    etSumTypes = cms.vint32(),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
--- L1Trigger/L1TZDC/l1tZDCAnalyzer_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from .L1TZDCAnalyzer import L1TZDCAnalyzer
-
-l1tZDCAnalyzer = L1TZDCAnalyzer()
