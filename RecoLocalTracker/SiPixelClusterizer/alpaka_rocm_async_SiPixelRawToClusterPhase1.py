2bd3235e800b4b4f1ef1e3386049e1979b47a88f
Cms Build <cmsbuild@cern.ch>
2025-10-07 12:19:29 +0200

    Updated for CMSSW_15_0_X_2025-10-07-1100

--- RecoLocalTracker/SiPixelClusterizer/alpaka_rocm_async_SiPixelRawToClusterPhase1.py
+++ RecoLocalTracker/SiPixelClusterizer/alpaka_rocm_async_SiPixelRawToClusterPhase1.py
@@ -10,6 +10,8 @@ def alpaka_rocm_async_SiPixelRawToClusterPhase1(*args, **kwargs):
     VCaltoElectronGain_L1 = cms.double(50),
     VCaltoElectronOffset = cms.double(-60),
     VCaltoElectronOffset_L1 = cms.double(-670),
+    DoDigiMorphing = cms.bool(False),
+    MaxFakesInModule = cms.uint32(2400),
     InputLabel = cms.InputTag('rawDataCollector'),
     Regions = cms.PSet(
       inputs = cms.optional.VInputTag,
@@ -17,6 +19,13 @@ def alpaka_rocm_async_SiPixelRawToClusterPhase1(*args, **kwargs):
       maxZ = cms.optional.vdouble,
       beamSpot = cms.optional.InputTag
     ),
+    barrelRegions = cms.vstring(
+      '1,1-12,1-2',
+      '1,1-12,7-8',
+      '2,1-28,1',
+      '2,1-28,8'
+    ),
+    endcapRegions = cms.vstring(),
     CablingMapLabel = cms.string(''),
     mightGet = cms.optional.untracked.vstring,
     alpaka = cms.untracked.PSet(
