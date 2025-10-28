06d42a405c2f13b5b2f8aaf693d0564c1e8b3f81
Cms Build <cmsbuild@cern.ch>
2025-10-28 03:58:30 +0100

    Updated for CMSSW_16_0_X_2025-10-27-2300

--- HeterogeneousTest/CUDAKernel/cudaTestKernelAdditionModule_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from .CUDATestKernelAdditionModule import CUDATestKernelAdditionModule
-
-cudaTestKernelAdditionModule = CUDATestKernelAdditionModule()
--- HeterogeneousTest/CUDAWrapper/cudaTestWrapperAdditionModule_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from .CUDATestWrapperAdditionModule import CUDATestWrapperAdditionModule
-
-cudaTestWrapperAdditionModule = CUDATestWrapperAdditionModule()
