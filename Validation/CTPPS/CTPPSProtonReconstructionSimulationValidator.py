2caf806754a4fd616e3e624d96fc12b5b2fa467f
Cms Build <cmsbuild@cern.ch>
2025-11-05 04:01:12 +0100

    Updated for CMSSW_16_0_X_2025-11-04-2300

--- Validation/CTPPS/CTPPSProtonReconstructionSimulationValidator.py
+++ Validation/CTPPS/CTPPSProtonReconstructionSimulationValidator.py
@@ -6,6 +6,10 @@ def CTPPSProtonReconstructionSimulationValidator(*args, **kwargs):
     lhcInfoPerLSLabel = cms.string(''),
     lhcInfoPerFillLabel = cms.string(''),
     useNewLHCInfo = cms.bool(False),
+    tagHepMCBeforeSmearing = cms.InputTag('generator', 'unsmeared'),
+    tagHepMCAfterSmearing = cms.InputTag('beamDivergenceVtxGenerator'),
+    tagRecoProtonsSingleRP = cms.InputTag('ctppsProtons', 'singleRP'),
+    tagRecoProtonsMultiRP = cms.InputTag('ctppsProtons', 'multiRP'),
     outputFile = cms.string('output.root'),
     verbosity = cms.untracked.uint32(0),
     mightGet = cms.optional.untracked.vstring
