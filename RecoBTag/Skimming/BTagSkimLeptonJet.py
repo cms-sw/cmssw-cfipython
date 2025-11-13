2cedc3040073dc04951ba88be91e9d1faae64813
Cms Build <cmsbuild@cern.ch>
2025-11-13 15:00:07 +0100

    Updated for CMSSW_16_0_X_2025-11-13-1100

--- RecoBTag/Skimming/BTagSkimLeptonJet.py
+++ RecoBTag/Skimming/BTagSkimLeptonJet.py
@@ -2,6 +2,16 @@ import FWCore.ParameterSet.Config as cms
 
 def BTagSkimLeptonJet(*args, **kwargs):
   mod = cms.EDFilter('BTagSkimLeptonJet',
+    CaloJet = cms.InputTag('iterativeCone5CaloJets'),
+    MinimumCaloJetPt = cms.double(20),
+    MinimumPtRel = cms.double(0),
+    LeptonType = cms.string(''),
+    Lepton = cms.InputTag(''),
+    MinimumNLeptonJet = cms.int32(1),
+    MaximumDeltaR = cms.double(0.4),
+    MaximumLeptonEta = cms.double(2.5),
+    MinimumLeptonPt = cms.double(6),
+    MaximumCaloJetEta = cms.double(3),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
