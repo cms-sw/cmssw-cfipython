aa0701eac69d4542a378cf3f8ae50da015c753c5
Cms Build <cmsbuild@cern.ch>
2025-11-21 00:28:08 +0100

    Updated for CMSSW_16_0_X_2025-11-20-2300

--- Validation/EcalClusters/EgammaBasicClusters.py
+++ Validation/EcalClusters/EgammaBasicClusters.py
@@ -2,6 +2,27 @@ import FWCore.ParameterSet.Config as cms
 
 def EgammaBasicClusters(*args, **kwargs):
   mod = cms.EDProducer('EgammaBasicClusters',
+    barrelBasicClusterCollection = cms.InputTag('hybridSuperClusters', 'hybridBarrelBasicClusters'),
+    enableEndcaps = cms.bool(True),
+    endcapBasicClusterCollection = cms.InputTag('multi5x5SuperClusters', 'multi5x5EndcapBasicClusters'),
+    hist_bins_Size = cms.int32(20),
+    hist_min_Size = cms.double(0),
+    hist_max_Size = cms.double(20),
+    hist_bins_Phi = cms.int32(181),
+    hist_min_Phi = cms.double(-3.14159),
+    hist_max_Phi = cms.double(3.14159),
+    hist_bins_Eta = cms.int32(91),
+    hist_min_Eta = cms.double(-2.5),
+    hist_max_Eta = cms.double(2.5),
+    hist_bins_ET = cms.int32(200),
+    hist_min_ET = cms.double(0),
+    hist_max_ET = cms.double(200),
+    hist_bins_NumRecHits = cms.int32(50),
+    hist_min_NumRecHits = cms.double(0),
+    hist_max_NumRecHits = cms.double(50),
+    hist_bins_R = cms.int32(55),
+    hist_min_R = cms.double(0),
+    hist_max_R = cms.double(175),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
--- Validation/EcalRecHits/EcalRecHitsValidation.py
+++ Validation/EcalRecHits/EcalRecHitsValidation.py
@@ -2,6 +2,15 @@ import FWCore.ParameterSet.Config as cms
 
 def EcalRecHitsValidation(*args, **kwargs):
   mod = cms.EDProducer('EcalRecHitsValidation',
+    verbose = cms.untracked.bool(False),
+    moduleLabelMC = cms.string('generatorSmeared'),
+    hitsProducer = cms.string('g4SimHits'),
+    EBrechitCollection = cms.InputTag('ecalRecHit', 'EcalRecHitsEB'),
+    EBuncalibrechitCollection = cms.InputTag('ecalMultiFitUncalibRecHit', 'EcalUncalibRecHitsEB'),
+    enableEndcaps = cms.untracked.bool(True),
+    EErechitCollection = cms.InputTag('ecalRecHit', 'EcalRecHitsEE'),
+    ESrechitCollection = cms.InputTag('ecalPreshowerRecHit', 'EcalRecHitsES'),
+    EEuncalibrechitCollection = cms.InputTag('ecalMultiFitUncalibRecHit', 'EcalUncalibRecHitsEE'),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
