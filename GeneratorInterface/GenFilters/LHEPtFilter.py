cb3aeeeaed9f399b3ab489943109f7d9745a98f5
Cms Build <cmsbuild@cern.ch>
2025-12-22 11:54:57 +0100

    Updated for CMSSW_15_1_X_2025-12-22-1100

--- GeneratorInterface/GenFilters/LHEPtFilter.py
+++ GeneratorInterface/GenFilters/LHEPtFilter.py
@@ -2,7 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 def LHEPtFilter(*args, **kwargs):
   mod = cms.EDFilter('LHEPtFilter',
+    selectedPdgIds = cms.vint32(),
+    ptMin = cms.double(0),
+    ptMax = cms.double(-1),
     isScalar = cms.bool(False),
+    src = cms.InputTag('externalLHEProducer'),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
