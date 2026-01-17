767df443257b1844c87c0d81dc7882cbf1d429be
Cms Build <cmsbuild@cern.ch>
2026-01-17 12:23:49 +0100

    Updated for CMSSW_16_1_X_2026-01-17-1100

--- CalibTracker/SiPixelESProducers/siPixelGainCalibrationForHLTGPU_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from .SiPixelGainCalibrationForHLTGPUESProducer import SiPixelGainCalibrationForHLTGPUESProducer
-
-siPixelGainCalibrationForHLTGPU = SiPixelGainCalibrationForHLTGPUESProducer()
--- CalibTracker/SiPixelESProducers/siPixelROCsStatusAndMappingWrapperESProducer_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from .SiPixelROCsStatusAndMappingWrapperESProducer import SiPixelROCsStatusAndMappingWrapperESProducer
-
-siPixelROCsStatusAndMappingWrapperESProducer = SiPixelROCsStatusAndMappingWrapperESProducer()
