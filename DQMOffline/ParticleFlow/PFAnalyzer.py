6986db8f4df7afcdcc35af268a41e47cb7616ab8
Cms Build <cmsbuild@cern.ch>
2026-01-30 12:01:47 +0100

    Updated for CMSSW_16_1_X_2026-01-30-1100

--- DQMOffline/ParticleFlow/PFAnalyzer.py
+++ DQMOffline/ParticleFlow/PFAnalyzer.py
@@ -2,6 +2,22 @@ import FWCore.ParameterSet.Config as cms
 
 def PFAnalyzer(*args, **kwargs):
   mod = cms.EDProducer('PFAnalyzer',
+    isMiniAOD = cms.bool(True),
+    pfCandidates = cms.InputTag('particleFlow'),
+    pfJetCollection = cms.InputTag('ak4PFJets'),
+    TriggerResultsLabel = cms.InputTag('TriggerResults', '', 'HLT'),
+    eventSelection = cms.string('nocut'),
+    TriggerNames = cms.vstring(),
+    PVCollection = cms.InputTag('offlinePrimaryVertices'),
+    pfAnalysis = cms.PSet(
+      observables = cms.vstring(),
+      eventObservables = cms.vstring(),
+      pfInJetObservables = cms.vstring(),
+      NPVBins = cms.vdouble(),
+      cutList = cms.vstring(),
+      binList2D = cms.vstring(),
+      jetCutList = cms.vstring()
+    ),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
