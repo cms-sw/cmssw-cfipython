80be6700ee0b8dbb8d4f8b9f776b39f371e9d748
Cms Build <cmsbuild@cern.ch>
2026-02-05 15:44:36 +0100

    Updated for CMSSW_16_1_X_2026-02-05-1100

--- Alignment/MillePedeAlignmentAlgorithm/MillePedeFileExtractor.py
+++ Alignment/MillePedeAlignmentAlgorithm/MillePedeFileExtractor.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 def MillePedeFileExtractor(*args, **kwargs):
   mod = cms.EDAnalyzer('MillePedeFileExtractor',
     fileDir = cms.string(''),
-    outputBinaryFile = cms.string('milleBinary%04d.dat'),
+    outputBinaryFile = cms.string('milleBinary%04d.dat.gz'),
     fileBlobInputTag = cms.InputTag('millePedeFileConverter'),
     maxNumberOfBinaries = cms.int32(1000),
     mightGet = cms.optional.untracked.vstring
