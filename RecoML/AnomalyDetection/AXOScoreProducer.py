d75ee443eab0727efc2c2db4f5e4b3d114ba251e
Cms Build <cmsbuild@cern.ch>
2026-03-12 02:47:02 +0100

    Updated for CMSSW_16_1_X_2026-03-11-2300

--- RecoML/AnomalyDetection/AXOScoreProducer.py
+++ RecoML/AnomalyDetection/AXOScoreProducer.py
@@ -1,14 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-def HLTADFilter(*args, **kwargs):
-  mod = cms.EDFilter('HLTADFilter',
+def AXOScoreProducer(*args, **kwargs):
+  mod = cms.EDProducer('AXOScoreProducer',
     pfCandidates = cms.InputTag('hltParticleFlow'),
     muons = cms.InputTag('hltIterL3MuonsNoVtx'),
     egammaCands = cms.InputTag('hltEgammaCandidates'),
     gsfTracks = cms.InputTag('hltEgammaGsfTracks'),
     vertices = cms.InputTag('hltPixelVertices'),
     modelPath = cms.FileInPath('HLTrigger/HLTfilters/data/hlt_ad_model.pt'),
-    threshold = cms.double(21.499275),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
