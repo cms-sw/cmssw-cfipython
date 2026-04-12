3bf2fa2683496d48e00ed0a974723d3353a4ee5c
Cms Build <cmsbuild@cern.ch>
2026-04-12 11:32:26 +0200

    Updated for CMSSW_17_0_X_2026-04-12-0000

---
+++ GeneratorInterface/Pythia8Interface/Pythia8HepMC3EGun.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+def Pythia8HepMC3EGun(*args, **kwargs):
+  mod = cms.EDFilter('Pythia8HepMC3EGun',
+    mightGet = cms.optional.untracked.vstring
+  )
+  for a in args:
+    mod.update_(a)
+  mod.update_(kwargs)
+  return mod
