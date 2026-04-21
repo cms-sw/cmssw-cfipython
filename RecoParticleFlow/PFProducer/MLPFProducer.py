2c6763b8bb23414b1f6512a8ef7c3e852a662c21
Cms Build <cmsbuild@cern.ch>
2026-04-21 16:05:08 +0200

    Updated for CMSSW_17_0_X_2026-04-21-1100

--- RecoParticleFlow/PFProducer/MLPFProducer.py
+++ RecoParticleFlow/PFProducer/MLPFProducer.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 def MLPFProducer(*args, **kwargs):
   mod = cms.EDProducer('MLPFProducer',
     src = cms.InputTag('particleFlowBlock'),
-    model_path = cms.FileInPath('RecoParticleFlow/PFProducer/data/mlpf/mlpf_2021_11_16__no_einsum__all_data_cms-best-of-asha-scikit_20211026_042043_178263.workergpu010.onnx'),
+    model_path = cms.FileInPath('RecoParticleFlow/PFProducer/data/mlpf/mlpf_5M_attn2x3x256_bm12_relu_checkpoint10_8xmi250_fp32_fused_20250722.onnx'),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
