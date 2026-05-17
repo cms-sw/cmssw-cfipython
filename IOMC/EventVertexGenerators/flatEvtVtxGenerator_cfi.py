bd9ab89927b8e06f079f40c57b6a0035259dbf36
Cms Build <cmsbuild@cern.ch>
2026-05-18 00:59:22 +0200

    Updated for CMSSW_17_0_X_2026-05-17-2300

--- IOMC/EventVertexGenerators/flatEvtVtxGenerator_cfi.py
+++ IOMC/EventVertexGenerators/flatEvtVtxGenerator_cfi.py
@@ -2,4 +2,4 @@ import FWCore.ParameterSet.Config as cms
 
 from .FlatEvtVtxGenerator import FlatEvtVtxGenerator
 
-FlatEvtVtxGenerator = FlatEvtVtxGenerator()
+flatEvtVtxGenerator = FlatEvtVtxGenerator()
