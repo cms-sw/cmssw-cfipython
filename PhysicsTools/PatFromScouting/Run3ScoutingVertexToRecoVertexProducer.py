9bb80dbd1c8af82d8cafdfa75d86f0ea8791e9ef
Cms Build <cmsbuild@cern.ch>
2026-06-11 11:57:52 +0200

    Updated for CMSSW_16_1_X_2026-06-11-1100

--- PhysicsTools/PatFromScouting/Run3ScoutingVertexToRecoVertexProducer.py
+++ PhysicsTools/PatFromScouting/Run3ScoutingVertexToRecoVertexProducer.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 def Run3ScoutingVertexToRecoVertexProducer(*args, **kwargs):
   mod = cms.EDProducer('Run3ScoutingVertexToRecoVertexProducer',
     src = cms.InputTag('hltScoutingPrimaryVertexPacker', 'primaryVtx'),
+    skipMissingProduct = cms.bool(False),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
