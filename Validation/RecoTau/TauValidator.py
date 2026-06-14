3f3584f76e8f23b784d4671592fcf5bad539eec7
Cms Build <cmsbuild@cern.ch>
2026-06-15 00:54:06 +0200

    Updated for CMSSW_17_0_X_2026-06-14-2300

---
+++ Validation/RecoTau/TauValidator.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+
+def TauValidator(*args, **kwargs):
+  mod = cms.EDProducer('TauValidator',
+    genTauCollection = cms.InputTag('tauGenJets'),
+    recoTauCollection = cms.InputTag('hltHpsPFTauProducer'),
+    recoTauIDCollections = cms.VInputTag(
+      'hltHpsPFTauDeepTauProducer:VSjet',
+      'hltHpsPFTauDeepTauProducer:VSe',
+      'hltHpsPFTauDeepTauProducer:VSmu'
+    ),
+    cutIDs_raw = cms.vdouble(
+      0,
+      0,
+      0
+    ),
+    cutIDs_wp = cms.vint32(
+      -1,
+      -1,
+      -1
+    ),
+    minDeltaR = cms.double(0.3),
+    outFolder = cms.string('HLT/Tau/TauValidation'),
+    isPatTaus = cms.untracked.bool(False),
+    mightGet = cms.optional.untracked.vstring
+  )
+  for a in args:
+    mod.update_(a)
+  mod.update_(kwargs)
+  return mod
---
+++ Validation/RecoTau/tauValidator_cfi.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+from .TauValidator import TauValidator
+
+tauValidator = TauValidator()
