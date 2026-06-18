d5b79a76743e5f63f11bf7a560ef9850280ea7f3
Cms Build <cmsbuild@cern.ch>
2026-06-18 05:03:51 +0200

    Updated for CMSSW_20_0_X_2026-06-17-2300

--- FWStorage/Services/ScitagConfig.py
+++ FWStorage/Services/ScitagConfig.py
@@ -3,14 +3,14 @@ import FWCore.ParameterSet.Config as cms
 def ScitagConfig(*args, **kwargs):
   mod = cms.Service('ScitagConfig',
     analysis = cms.untracked.PSet(
-      primarySciTag = cms.untracked.uint32(196664),
-      embeddedSciTag = cms.untracked.uint32(196700),
-      preMixedPileupSciTag = cms.untracked.uint32(196704)
+      primarySciTag = cms.untracked.uint32(206),
+      embeddedSciTag = cms.untracked.uint32(215),
+      preMixedPileupSciTag = cms.untracked.uint32(216)
     ),
     production = cms.untracked.PSet(
-      primarySciTag = cms.untracked.uint32(196656),
-      embeddedSciTag = cms.untracked.uint32(196700),
-      preMixedPileupSciTag = cms.untracked.uint32(196704)
+      primarySciTag = cms.untracked.uint32(204),
+      embeddedSciTag = cms.untracked.uint32(215),
+      preMixedPileupSciTag = cms.untracked.uint32(216)
     ),
     enable = cms.untracked.bool(True),
     productionCase = cms.untracked.bool(False)
