f1dc19f40a9305f6d8ec28e4eb0c02f5f9775eff
Cms Build <cmsbuild@cern.ch>
2026-07-01 01:08:32 +0200

    Updated for CMSSW_17_0_X_2026-06-30-2300

--- L1Trigger/Phase2L1GMT/Phase2L1TGMTFwdMuonTranslator.py
+++ L1Trigger/Phase2L1GMT/Phase2L1TGMTFwdMuonTranslator.py
@@ -4,7 +4,8 @@ def Phase2L1TGMTFwdMuonTranslator(*args, **kwargs):
   mod = cms.EDProducer('Phase2L1TGMTFwdMuonTranslator',
     stubs = cms.InputTag('gmtStubs'),
     emtfTracks = cms.InputTag('simEmtfDigisPhase2'),
-    omtfTracks = cms.InputTag('simOmtfPhase2Digis'),
+    omtfConstrSAMs = cms.InputTag('simOmtfPhase2Digis', 'constrCands'),
+    omtfUnconstrSAMs = cms.InputTag('simOmtfPhase2Digis', 'unconstrCands'),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
