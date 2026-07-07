71d95e3e85f8742c63e3e8c2aa1def427bfb6d7d
Cms Build <cmsbuild@cern.ch>
2026-07-07 14:27:22 +0200

    Updated for CMSSW_20_1_X_2026-07-07-1100

--- FWCore/Integration/ConcurrentIOVESSource.py
+++ FWCore/Integration/ConcurrentIOVESSource.py
@@ -3,7 +3,6 @@ import FWCore.ParameterSet.Config as cms
 def ConcurrentIOVESSource(*args, **kwargs):
   mod = cms.ESSource('ConcurrentIOVESSource',
     iovIsRunNotTime = cms.bool(True),
-    concurrentFinder = cms.bool(True),
     testForceESSourceMode = cms.bool(False),
     findForRecordA = cms.bool(False),
     firstValidLumis = cms.vuint32(),
