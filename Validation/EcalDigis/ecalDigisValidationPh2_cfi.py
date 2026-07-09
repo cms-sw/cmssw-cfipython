3139c63350abdd29926817a7849ee787063e5a9d
Cms Build <cmsbuild@cern.ch>
2026-07-09 13:47:39 +0200

    Updated for CMSSW_20_1_0_pre1

--- Validation/EcalDigis/ecalDigisValidationPh2_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from .EcalDigisValidationPh2 import EcalDigisValidationPh2
-
-ecalDigisValidationPh2 = EcalDigisValidationPh2()
--- Validation/EcalTriggerPrimitives/ecalTPsValidationPh2_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from .EcalTPsValidationPh2 import EcalTPsValidationPh2
-
-ecalTPsValidationPh2 = EcalTPsValidationPh2()
