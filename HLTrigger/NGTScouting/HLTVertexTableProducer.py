95e92b225ebaf35672981eaa524a1b9b8ee7cd64
Cms Build <cmsbuild@cern.ch>
2026-07-30 03:25:51 +0200

    Updated for CMSSW_20_1_X_2026-07-29-2300

--- HLTrigger/NGTScouting/HLTVertexTableProducer.py
+++ HLTrigger/NGTScouting/HLTVertexTableProducer.py
@@ -3,13 +3,18 @@ import FWCore.ParameterSet.Config as cms
 def HLTVertexTableProducer(*args, **kwargs):
   mod = cms.EDProducer('HLTVertexTableProducer',
     skipNonExistingSrc = cms.bool(False),
-    pvName = cms.required.string,
-    pvSrc = cms.required.InputTag,
     usePF = cms.bool(True),
-    pfSrc = cms.required.InputTag,
-    goodPvCut = cms.required.string,
-    dlenMin = cms.required.double,
-    dlenSigMin = cms.required.double,
+    doSVs = cms.bool(True),
+    pvName = cms.string('hltPrimaryVertex'),
+    pvSrc = cms.InputTag('hltOfflinePrimaryVertices'),
+    pfSrc = cms.InputTag('hltParticleFlowTmp'),
+    goodPvCut = cms.string(''),
+    svName = cms.string('hltSecondaryVertex'),
+    svDoc = cms.string('secondary vertices from IVF algorithm'),
+    svSrc = cms.InputTag('hltDeepInclusiveSecondaryVerticesPF'),
+    goodSvCut = cms.string(''),
+    dlenMin = cms.double(0),
+    dlenSigMin = cms.double(3),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
