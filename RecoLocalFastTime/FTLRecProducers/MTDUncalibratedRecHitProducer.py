00af5d375acc3e33bac76fb97e29b67d9a88ee18
Cms Build <cmsbuild@cern.ch>
2026-07-31 12:06:19 +0200

    Updated for CMSSW_20_0_X_2026-07-31-1100

--- RecoLocalFastTime/FTLRecProducers/MTDUncalibratedRecHitProducer.py
+++ RecoLocalFastTime/FTLRecProducers/MTDUncalibratedRecHitProducer.py
@@ -2,6 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 def MTDUncalibratedRecHitProducer(*args, **kwargs):
   mod = cms.EDProducer('MTDUncalibratedRecHitProducer',
+    barrelDigis = cms.InputTag('mix', 'FTLBarrel'),
+    endcapDigis = cms.InputTag('mix', 'FTEndcap'),
+    BarrelHitsName = cms.string('FTLBarrel'),
+    EndcapHitsName = cms.string('FTLEndcap'),
+    barrel = cms.PSet(),
+    endcap = cms.PSet(),
     mightGet = cms.optional.untracked.vstring
   )
   for a in args:
---
+++ RecoLocalFastTime/FTLRecProducers/mtdUncalibratedRecHitProducer_cfi.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+from .MTDUncalibratedRecHitProducer import MTDUncalibratedRecHitProducer
+
+mtdUncalibratedRecHitProducer = MTDUncalibratedRecHitProducer()
